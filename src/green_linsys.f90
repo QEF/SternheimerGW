3b09243e1011c2ac790d5c30663c0766c8430e89
Montmorency <lamberh@tcd.ie>
2011-07-05 16:46:00 +0000

    Committing at stage where TPA preconditioner is implemented in green_linsys.f90 and green's fxn has been tested vs. SOS and EMP implementations. With 5 Ryd everything converges, with 18 Ryd above w = 30ev things don't converge however the shape of the Green's function is still correct. I have also done tests of USPP and NC coulomb routine calculating static di-electric constant and compared to literature see groupresentations 062811. I've done a full 5 Ryd runthrough with NC pseudo and model dielec fxn but need to compare it to EMP calculation with same parameters.
    git-svn-id: https://rothery3.materials.ox.ac.uk/svn/sgw/branches/lambert/trunk@281 21dd5d8e-1f79-4369-87ad-170e0d29e828

--- src/green_linsys.f90
+++ src/green_linsys.f90
@@ -1,5 +1,4 @@
 SUBROUTINE green_linsys(ik0, iq)
-
 ! SGW: call green_linsys ( vr, g2kin, k0mq, nwgreen, wgreen, igstart, igstop, ik0, iq )
 ! "In order to calculate the analytic component G^{A} we consider G^{A}(r,r',w) as a
 ! parametric function of the first space variable and of the frequency: G^{A}_[r,w](r') 
@@ -55,9 +54,14 @@ SUBROUTINE green_linsys(ik0, iq)
 
   real(DP) :: thresh, anorm, averlt, dr2
   logical :: conv_root
+
   !HL Again need to think about the dimensions of the green's fxn.
-  COMPLEX(DP) :: gr_A(npwx), rhs(npwx)
-  COMPLEX(DP) :: gr_N(npwx), gr(npwx), ci, cw, green(ngmsig,ngmsig)
+  !It is a matrix of the dimension of the plane wave description before
+  !truncation. The second index is a dummy band index although it could come in handy
+  !if we started to look at spin systems.  
+
+  COMPLEX(DP) :: gr_A(npwx,1), rhs(npwx, 1)
+  COMPLEX(DP) :: gr_N(npwx,1), gr(npwx,1), ci, cw, green(ngmsig,ngmsig)
   COMPLEX(DP), ALLOCATABLE :: etc(:,:)
 
   INTEGER :: iw, igp
@@ -94,54 +98,63 @@ SUBROUTINE green_linsys(ik0, iq)
 
 !Parameter for the dirac delta function
 !real(DP), parameter :: eta = 0.04
-
 !@10TION
 !HL need a threshold here for the linear system solver. This could also go in the punch card
 !with some default at a later date. 
 
         REAL(DP) :: tr_cgsolve = 1.0d-5
-
         allocate (h_diag ( npwx*npol, nbnd))
         allocate (etc(nbnd, nkstot))
   
- !dummy array
+!dummy array
   
         ci = (0.0d0, 1.0d0)
-        eta = 0.1d0
+        eta = 0.04d0
 
 !convert freq array generated in freqbins into rydbergs.
         w_ryd(:) = wgreen(:)/RYTOEV
+        CALL start_clock('greenlinsys')
 
-!       write(6,*) nwgreen
-!       do iw = 1, nwgreen
-!       write(6,*) w_ryd(iw), wgreen(iw)
-!       enddo
-!       this is now set in prepare_kmq
-!       k0mq(:) = xk(:,ik0) - xq(:) the wave functions at k0mq should be stored in:
-!       if (.not.lgamma.and.nksq.gt.1) then
-!          read (iunigk, err = 200, iostat = ios) npwq, igkq
-!200       call errore ('solve_linter', 'reading igkq', abs (ios) )
-!       endif
-!  Write this wave function \psi_{k-q} to file to be used again later when I need to form the 
-!  G^{NA}v(r,r') component of the self-energy.
-!  call davcio (evq, lrwfc, iuwfc, ikq, -1)
-!     do ibnd = 1,1
-!      do ig=1,npwx
-!          write(6,*)evq(ig, ibnd)
-!      enddo
-!     enddo
-
-CALL start_clock('greenlinsys')
-
-!WRITE( 6, '(4x,"k0-q = (",3f12.7," )",10(3x,f7.3))') xq, et(:,ikq)*RYTOEV
+! WRITE( 6, '(4x,"k0-q = (",3f12.7," )",10(3x,f7.3))') xq, et(:,ikq)*RYTOEV
 ! generally the eigenvalues/for each kpoint we want will be stored somewhere in the array xk
 ! i.e. if we want to look at a specific k-point we need to know where it sits in that array.
+
 ! for now I am only interested in k = Gamma, and therefore k-q will always be in the second spot in all these arrays.
+! When I want to start looking at a few k-points in the BZ then things might become a little bit trickier and 
+! could try to foil me. 
+
 ! ikks(ik) = 2*ik. ikqs(ik) = 2*ik. ik=1=Gamma.
 
-       ikq = 2
+!Again
+!        ikk = ikks(ik)
+!        ikq = ikqs(ik)
 
-       call davcio (evq, lrwfc, iuwfc, ikq, - 1)
+        ikq = 2
+        call davcio (evq, lrwfc, iuwfc, ikq, - 1)
+
+! Various plane wave variables.
+! I never checked to make sure that the eigenfxns, kinetic energy, pseudo-potentials, 
+! form factors etc. were being generated properly. This is a very silly state of affairs.
+! Tomorrow need to check all these things carefully so that green_linsys is a bit more like
+! solve_linter. But it certainly appears necessary to have these variables set properly for
+! when h_psiq is called and the linear system is run. 
+
+        if (nksq.gt.1) rewind (unit = iunigk)
+
+        if (nksq.gt.1) then
+           read (iunigk, err = 100, iostat = ios) npw, igk
+100        call errore ('green_linsys', 'reading igk', abs (ios) )
+        endif
+
+        if (.not.lgamma.and.nksq.gt.1) then
+           read (iunigk, err = 200, iostat = ios) npwq, igkq
+200        call errore ('green_linsys', 'reading igkq', abs (ios) )
+        endif
+
+        !   Calculates beta functions (Kleinman-Bylander projectors), with
+        !   structure factor, for all atoms, in reciprocal space
+
+       call init_us_2 (npwq, igkq, xk (1, ikq), vkb)
 
        do ig = 1, npwq
            g2kin (ig) = ( (xk (1,ikq) + g (1, igkq(ig)) ) **2 + &
@@ -162,35 +175,27 @@ CALL start_clock('greenlinsys')
        enddo
 
 !@10TION
-! Here it becomes important that all the valence band eigenvalues are < 0 for the frequency formalism etc.
-  
-    et(:,2) = et(:,2) - (6.58D0/13.605)
+! Here it becomes important that all the valence band eigenvalues are < 0.
+!  
+! et(:,:) = et(:,:) - (6.58D0/13.605)
 
 WRITE( 6, '(4x,"k0-q = (",3f12.7," )",10(3x,f7.3))') xq, et(:,2)*RYTOEV
 DO iw = 1, nwgreen
     green =(0.0d0, 0.0d0)
-!HL here we parallelize over Gs  do ig = igstart, igstop
-   do ig = 1, ngmsig !zero after this point
-
-       rhs(:)  = (0.0d0, 0.0d0)
-       rhs(ig) = -(1.0d0, 0.0d0)
-       gr_A(:) = (0.0d0, 0.0d0) 
+!    do ig = 1, ngmsig !zero after this point
+     do ig = 1, 1 
+       rhs(:,:)  = (0.0d0, 0.0d0)
+       rhs(ig,1) = -(1.0d0, 0.0d0)
+       gr_A(:,:) = (0.0d0, 0.0d0) 
        conv_root = .true.
        lter = 0
-       anorm = 0.0d0
 
-! Going to use same trick as in solve linter where we combine the frequency and the eigenvalue 
-! and in this case a small imaginary component
-! HL 05/07/11 except in the expressions for the analytic component of the green's function the eigenvalue
-! doesn't show up anywhere.
-
+!      anorm = 0.0d0
        etc(:, :) = CMPLX( 0.0d0, 0.0d0, kind=DP)
-       cw = CMPLX( w_ryd(iw), 0.2d0, kind=DP)
+       cw = CMPLX( w_ryd(iw), eta, kind=DP)
+!      cw = CMPLX( 0.0d0, w_ryd(iw), kind=DP)
 
 !      cw = CMPLX( 0.0d0, w_ryd(iw), kind=DP)
-!      cw(1) = CMPLX( w_ryd(iw), eta, kind=DP)
-!      npwq should be the number of plane waves that describes this wave function.
-!      in SGW nbnd_occ is set to 1
 !      call cbcg_solve(cch_psi_all, cg_psi, etc(:,ikq), rhs, gr_A, h_diag, &
 !           npwx, npwq, tr_cgsolve, ikq, lter, conv_root, anorm, 1, npol, .false.)
 !      call cbcg_solve_fix(cch_psi_all_fix, cg_psi, etc, rhs, gr_A, h_diag, &
@@ -198,47 +203,44 @@ DO iw = 1, nwgreen
 !      call cgsolve_all (ch_psi_all, cg_psi, w_ryd(iw), rhs, gr_A, &
 !                         h_diag, npwx, npwq, 1.0d-2, ikq, lter, conv_root, &
 !                         anorm, nbnd_occ(ikq), npol )
-! green_linsys in sgw passes cdummy as the values for eqv: i.e. 
-! the occupied manifold is set to zero...
-! i.e. all the projectors onto the empty state should be zero.
-! also ikq is set to zero.
 
        call cbcg_solve_fix(cch_psi_all_green, cg_psi, etc(1,ikq), rhs, gr_A, h_diag, &
-             npwx, npwq, tr_cgsolve, ikq, lter, conv_root, anorm, 1, npol, cw, .true.)
+             npwx, npwq, tr_cgsolve, ikq, lter, conv_root, anorm, 1, npol, cw, .false.)
 
-!       IF (.not.conv_root) then
-!         WRITE(6,'(" Root Not Converged ")')
-!         STOP
-!         WRITE( stdout, '(/,5x," iter # ",i3," anorm :",f8.3, &
-!         " gr_A(1): ",f8.3)') lter, anorm, real(gr_A(ig))
-!       ENDIF
-!      WRITE(6,*) iw, ig, gr_A(1)
-!      Evaluate the G_{N} part of the Green's function.
+       IF (.not.conv_root) then
+        WRITE(6,'(" Root Not Converged ")')
+!       WRITE( stdout, '(/,5x," iter # ",i3," anorm :",f8.3, &
+!      "gr_A(1): ",f8.3)') lter, anorm, real(gr_A(ig))
+!       gr_A = (0.d0, 0.0d0)
+       ENDIF
 
-      gr_N(:) = (0.0d0, 0.0d0)
+      gr_N(:,1) = (0.0d0, 0.0d0)
 
       do ibnd = 1, 4
 !       x = w_ryd(iw) - eval(ibnd)
         x = w_ryd(iw) - et(ibnd, ikq)
+!        x = (0.0d0, 1.0d0)*w_ryd(iw) - et(ibnd, ikq)
         dirac = eta / pi / (x**2.d0 + eta**2.d0)
 
 !      no spin factor (see above) 
-!HL    I might need to divide this by two since q-espresso has the spin factor in the kweights.
+! HL   I might need to divide this by two since q-espresso has the spin factor in the kweights.
 !      gr_N = gr_N + 2.d0 * twopi * ci * conjg ( psi(ig,ibnd) ) * psi(1:ngm,ibnd) * dirac
 !      gr_N = gr_N + tpi * ci * conjg( evq(ig,ibnd) ) * evq(1:ngm,ibnd) * dirac
 !      do igp = 1, npwq
 
        do igp = 1, ngmsig
-!      SGW  gr_N(ig) = gr_N(ig) + tpi * ci * conjg( evq(ig,ibnd) ) * evq(igp,ibnd) * dirac
-            gr_N(ig) = gr_N(ig) + tpi * ci * conjg(evq(ig,ibnd)) *  evq(igp,ibnd) * dirac
+!       do igp = 1, 1
+!       SGW  gr_N(ig) = gr_N(ig) + tpi * ci * conjg( evq(ig,ibnd) ) * evq(igp,ibnd) * dirac
+             gr_N(ig,1) = gr_N(ig,1) + tpi * ci * conjg(evq(ig,ibnd)) *  evq(igp,ibnd) * dirac
        enddo
       enddo
-      gr = gr_A + gr_N
-!     write(6,'(3f15.10)') gr_A
+
+       gr = gr_A + gr_N
+!       gr = gr_A
+
       do igp = 1, ngmsig
-         green (ig,igp) = gr (igp)
-!        write(6, '(3f15.10)') green (ig, igp)
-!        if (ig.eq.1.and.igp.eq.1) write(6,'(3f15.10)') wgreen(iw), green(ig,igp)
+         green (ig,igp) = gr (igp,1)
+         if (ig.eq.1.and.igp.eq.1) write(6,'(3f15.10)') wgreen(iw), green(ig,igp)
       enddo
    enddo
 
@@ -249,14 +251,13 @@ DO iw = 1, nwgreen
 !    if (me.eq.1.and.mypool.eq.1) then
 !#endif
 !  rec0 = (iw-1) * nk0 * nq + (ik0-1) * nq + (iq-1) + 1
-
    rec0 = (iw-1) * 1 * nqs + (ik0-1) * nqs + (iq-1) + 1
    write ( iungreen, rec = rec0, iostat = ios) green
-
 !#ifdef __PARA
 !    endif
 !#endif
 ENDDO !enddo on iw
+STOP
 CALL stop_clock('greenlinsys')
 RETURN
 END SUBROUTINE green_linsys
