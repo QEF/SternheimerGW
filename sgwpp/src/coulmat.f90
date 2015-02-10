10bf4934e5bab2a4fb23f8d0112afceee1a8ed2e
Montmorency <lamberh@tcd.ie>
2015-02-10 19:09:45 +0000

    going to add symmetry routines
    git-svn-id: https://rothery3.materials.ox.ac.uk/svn/sgw/branches/sgw-5.0@502 21dd5d8e-1f79-4369-87ad-170e0d29e828

--- sgwpp/src/coulmat.f90
+++ sgwpp/src/coulmat.f90
@@ -120,8 +120,8 @@ END MODULE
 MODULE units_coulmat
   ! ... the units of the files and the record lengths
   SAVE
-  INTEGER :: iuncoulmat
-  INTEGER :: lrcoulmat
+  INTEGER :: iuncoulmat, iuncoul
+  INTEGER :: lrcoulmat, lrcoul
 END MODULE units_coulmat
 
 MODULE dielectric
@@ -133,9 +133,9 @@ END MODULE dielectric
 MODULE control_coulmat
   USE kinds,    ONLY : DP
   SAVE
-  LOGICAL  :: do_coulmat, do_fsavg
-  INTEGER  :: nbndmin, nbndmax
-  REAL(DP) :: degaussfs
+  LOGICAL  :: do_coulmat, do_fsavg, do_lind
+  INTEGER  :: nbndmin, nbndmax, ngcoul
+  REAL(DP) :: degaussfs, debye_e
 END MODULE control_coulmat
 
 !------------------------------
@@ -158,14 +158,13 @@ PROGRAM mustar
   USE mp_global,   ONLY : mp_startup
   USE environment, ONLY : environment_start
   USE klist,       ONLY : nks, nkstot, degauss,xk,wk
-  USE start_k,     only : nks_start, xk_start, wk_start, &
+  USE start_k,     ONLY : nks_start, xk_start, wk_start, &
                           nk1, nk2, nk3, k1, k2, k3
-  USE units_coulmat,  ONLY : iuncoulmat, lrcoulmat
-  USE dielectric,     ONLY : qtf, kf
-  USE control_coulmat, ONLY : do_coulmat, do_fsavg, nbndmin, degaussfs
-
+  USE units_coulmat,   ONLY : iuncoulmat, lrcoulmat, lrcoul, iuncoul
+  USE dielectric,      ONLY : qtf, kf
+  USE control_coulmat, ONLY : do_coulmat, do_fsavg, nbndmin, degaussfs, ngcoul, do_lind, &
+                              debye_e
   IMPLICIT NONE
-
   CHARACTER(LEN=256), EXTERNAL :: trimcheck
   !
   ! input variables
@@ -176,7 +175,7 @@ PROGRAM mustar
   CHARACTER(10)           :: calculation,smeartype
   LOGICAL                 :: metalcalc, exst
   !
-  NAMELIST / inputpp / prefix, outdir, calculation, nk1, nk2, nk3, qtf, do_coulmat, do_fsavg, nbndmin, kf, degaussfs
+  NAMELIST / inputpp / prefix, outdir, calculation, nk1, nk2, nk3, qtf, do_coulmat, do_fsavg, nbndmin, kf, degaussfs, ngcoul, do_lind, debye_e
   NAMELIST / energy_grid / smeartype,intersmear,intrasmear,wmax,wmin,nbndmax,nw,shift,nshell,eta,ibndmin,ibndmax, qmod_par
   !
   ! local variables
@@ -218,10 +217,13 @@ PROGRAM mustar
   metalcalc    = .false.
   do_coulmat   = .true.
   do_fsavg     = .true.
+  do_lind      = .false.
   qtf          = 1.0
   kf           = 1.0
   degaussfs    = 0.05
- !SHOULD READ FROM INPUT FILE
+  debye_e      = 0.0026
+
+  !SHOULD READ FROM INPUT FILE
   CALL input_from_file( )
   !
   ! read input file
@@ -314,17 +316,21 @@ PROGRAM mustar
   lrcoulmat  = 2 * nks * nks * nbnd * nbnd
   CALL diropn (29, 'coulmat', lrcoulmat, exst)
 
+!OPEN COULOMB directory
+  iuncoul = 28
+  lrcoul = 2 * ngcoul * ngcoul
+  CALL diropn (iuncoul, 'coul', lrcoul, exst)
+
   print *,"nk1 nk2 nk3", nk1, nk2, nk3
   print *,"Thomas-Fermi Vector", qtf
 
-  !Calculate coulomb matrix elements stored in struct vcnknpkp
+  !Calculate Coulomb matrix elements stored in struct vcnknpkp
   IF(do_coulmat) THEN
     IF (ionode) WRITE( stdout, "( 5x, 'Calculating Coulomb Matrix Elements' ) " )
         CALL coulmats(vcnknpkp)
   ENDIF
 
-
-  !Perform fermi surface averaging of matrix elements in vcnknpkp
+  !Perform Fermi surface averaging of matrix elements in vcnknpkp
   IF(do_fsavg) THEN
     IF (ionode) WRITE( stdout, "( 5x, 'Performing Fermi Surface average' ) " )
       CALL fsaverage()
@@ -335,6 +341,8 @@ PROGRAM mustar
   END SELECT
   !
   CALL stop_clock( 'calculation' )
+
+  CALL print_clock( 'calculation' )
   !
   IF ( ionode ) WRITE( stdout, *  )
 
@@ -363,8 +371,8 @@ SUBROUTINE coulmats(vcnknpkp)
   USE funct,                ONLY : xc
   USE grid_subroutines,     ONLY : realspace_grids_info
   USE buffers,              ONLY : save_buffer, get_buffer
-  USE units_coulmat,        ONLY : iuncoulmat, lrcoulmat
-  USE control_coulmat,      ONLY : nbndmin
+  USE units_coulmat,        ONLY : iuncoulmat, lrcoulmat, iuncoul, lrcoul
+  USE control_coulmat,      ONLY : nbndmin, ngcoul, do_lind
 
 IMPLICIT NONE
 
@@ -376,45 +384,65 @@ COMPLEX(DP) :: psink(dffts%nnr,nbnd), psinpkp(dffts%nnr,nbnd), psi_temp(dffts%nn
 COMPLEX(DP), ALLOCATABLE :: vc(:,:)
 INTEGER     :: ik, ikp, ibnd, jbnd, ig, igp
 INTEGER     :: nkp, nkp_abs, ipool
-INTEGER     :: ngcoul, ir, iq 
+INTEGER     :: ir, iq 
 LOGICAL     :: limq, exst
 COMPLEX(DP) :: evc1(npwx,nbnd)
 
-ngcoul = 200
 ALLOCATE(vc(ngcoul,ngcoul))
 !For testing use lindhard function
-vc    = (0.0d0,0.0d0)
 vcnknpkp = (0.0d0,0.0d0)
 ipool = 0
-!SHOULD READ FROM INPUT
+
 do iq = 1, nks
 !Screened Coulomb interaction
    xq(:) = xk(:,iq)
-   print*, "xq:  ", xq, iq
-   do ig =1, ngcoul
-    qg2 = (g(1,ig) + xq(1))**2 + (g(2,ig) + xq(2))**2 + (g(3,ig)+xq(3))**2
-    limq = (qg2.lt.eps8) 
-    if(.not.limq) then
-      vc(ig,ig) = (1.0d0/lind_eps(qg2))*dcmplx(e2*fpi/(tpiba2*qg2), 0.0d0)
-    endif
+   vc    = (0.0d0,0.0d0)
+   if (mod(iq,30).eq.0) print*, "xq:  ", iq
+   if(.not.do_lind) then 
+      print*, "reading xq:  ", xq, iq
+      CALL davcio (vc, lrcoul, iuncoul, iq, -1)
+!what is stored is: eps^-1(q)-1
+      do ig = 1, ngcoul
+         vc(ig,ig) = vc(ig,ig) + 1.0d0
+      enddo 
+   endif
+
+   do ig = 1, ngcoul
+      qg2 = (g(1,ig) + xq(1))**2 + (g(2,ig) + xq(2))**2 + (g(3,ig)+xq(3))**2
+      limq = (qg2.lt.eps8) 
+      if (limq) cycle
+      if (.not.do_lind) then
+         do igp = 1, ngcoul
+            vc(ig,igp) = vc(ig,igp)*dcmplx(e2*fpi/(tpiba2*qg2), 0.0d0)
+         enddo
+      else
+            vc(ig,ig) = (1.0d0/lind_eps(qg2))*dcmplx(e2*fpi/(tpiba2*qg2), 0.0d0)
+      endif
    enddo
+
+!nks should be local pool index for kpoints.!
+!need to make sure the loop is only over the local k-points...
    do ik = 1, nks
       CALL ktokpmq (xk(1,ik), xq(1), -1, ipool, nkp, nkp_abs)
       ikp = nkp
       xkp(:) = xk(:,ik)-xq(:)
+
 !read in wavefunctions at k: 
       psink(:,:)   = (0.0d0,0.0d0)
       psinpkp(:,:) = (0.0d0,0.0d0)
 
       CALL gk_sort (xk (1, ik), ngm, g, ecutwfc / tpiba2, npw, igk, g2kin)
       CALL davcio (evc, 2*nwordwfc, iunwfc, ik, -1 )
+
 !HL SHOULD ONLY STORE BANDS AROUND FERMI LEVEL
       psink(nls(igk(1:npw)), :) = evc(1:npw,:)
+
 !read in wavefunctions at k' = k-q: 
       CALL gk_sort (xkp(1), ngm, g, ecutwfc / tpiba2, npw, igk, g2kin)
       CALL davcio (evc, 2*nwordwfc, iunwfc, ikp, -1 )
       psinpkp(nls(igk(1:npw)),:) = evc(1:npw,:)
-!LOOP over bands calculated f functions etc.
+
+!LOOP over bands calculate f functions etc.
       do ibnd = nbndmin, nbnd
          CALL invfft ('Wave', psink(:,ibnd), dffts)
          do jbnd = nbndmin, nbnd
@@ -427,19 +455,25 @@ do iq = 1, nks
                fnknpkp(ir) = fnknpkp(ir) +  conjg(psi_temp(ir))*psink(ir,ibnd)
             enddo
             CALL fwfft ('Wave', fnknpkp(:), dffts)
-            do ig = 1, ngcoul 
-!nolocal fields do igp = 1, ngcoul
-               vcnknpkp(ik, ikp, ibnd, jbnd) = vcnknpkp(ik, ikp, ibnd, jbnd) + &
-                                               conjg(fnknpkp(nls(ig)))*vc(ig,ig)*fnknpkp(nls(ig))
-!               enddo
-            enddo
-        enddo
-     enddo!ibnd
-   enddo
-enddo
-
+            if(.not.do_lind) then
+              do ig = 1, ngcoul 
+                 do igp = 1, ngcoul
+                   vcnknpkp(ik, ikp, ibnd, jbnd) = vcnknpkp(ik, ikp, ibnd, jbnd) + &
+                                                   conjg(fnknpkp(nls(ig)))*vc(ig,igp)*fnknpkp(nls(ig))
+                 enddo
+              enddo
+            else
+              do ig = 1, ngcoul 
+                   vcnknpkp(ik, ikp, ibnd, jbnd) = vcnknpkp(ik, ikp, ibnd, jbnd) + &
+                                                   conjg(fnknpkp(nls(ig)))*vc(ig,ig)*fnknpkp(nls(ig))
+              enddo
+            endif
+         enddo!jbnd
+      enddo!ibnd
+   enddo!look xk
+enddo!loop xq
+!write matrix elements to file:
 CALL davcio (vcnknpkp, lrcoulmat, iuncoulmat, 1,+1)
-
 END SUBROUTINE coulmats
 
 SUBROUTINE fsaverage()
@@ -459,7 +493,7 @@ SUBROUTINE fsaverage()
                                    nk1, nk2, nk3, k1, k2, k3
   USE io_files,             ONLY : nwordwfc, iunwfc, diropn
   USE units_coulmat,        ONLY : iuncoulmat, lrcoulmat
-  USE control_coulmat,      ONlY : degaussfs, nbndmin
+  USE control_coulmat,      ONlY : degaussfs, nbndmin, debye_e
 
 IMPLICIT NONE
 
@@ -473,50 +507,39 @@ IMPLICIT NONE
 !IO
   LOGICAL     :: exst
 !to put in coul struct:
-  REAL(DP)    :: mu, mustar, debye_e
-
-
+  REAL(DP)    :: mu, mustar
   CALL davcio (vcnknpkp, lrcoulmat, iuncoulmat, 1,-1)
-
-!First calculate dosef 
-   !dosef = dos_ef (ngaussw, degaussw0, ef0, etf, wkf, nksf, nbndsub)
-   !   N(Ef) in the equation for lambda is the DOS per spin
-   !dosef = dosef / two
-   tetra=.true.
-    WRITE( stdout,'(/5x,"Gaussian broadening (read from input): ",&
-        &        "ngauss,degauss=",i4,f12.6/)') ngauss, degauss
-
-!CALCULATE Dos at Fermi Level gaussian:
+  !First calculate dosef: 
+  !dosef = dos_ef (ngaussw, degaussw0, ef0, etf, wkf, nksf, nbndsub)
+  !N(Ef) in the equation for lambda is the DOS per spin
+  !dosef = dosef / two
+  WRITE( stdout,'(/5x,"Gaussian broadening (read from input): ",&
+       &        "ngauss,degauss=",i4,f12.6/)') ngauss, degauss
+  !CALCULATE Dos at Fermi Level gaussian:
    CALL dos_g(et,nspin,nbnd, nks,wk,degauss,ngauss, ef, DOSofE)
-!use tetrahedron method:
+  !tetra=.true.
+  !use tetrahedron method:
   !CALL dos_t(et, nspin, nbnd, nks, ntetra, tetra, ef, DOSofE)
   !want density of states per spin.
-   N0 = DOSofE(1)/2.d0
-   !degaussw0 = 0.025 !eV
-   !degaussw0 = degaussw0/RYTOEV !eV
+  N0 = DOSofE(1)/2.d0
   if (degaussfs.eq.0.0d0) then
      degaussw0 = degauss
   else
      degaussw0 = degaussfs
   endif
-
-   En      = ef
-   nqs     = dble(nk1*nk2*nk3)
-   print*, ef*rytoev, N0/rytoev
-   print*, "degauss fs"
+  En      = ef
+  nqs     = dble(nk1*nk2*nk3)
   mu = 0.0d0
+
   do ik = 1, nks
    if (mod(ik,20).eq.0) print*, "xk:  ", ik
    do ikp = 1, nks
-!  do ik = ikstart, ikstop
-!   do ikp = ikstart, ikstop
     do ibnd = nbndmin, nbnd
      do jbnd = nbndmin, nbnd
         enk = (et(ibnd, ik) - ef)
         enpkp = (et(jbnd, ikp) - ef)
         w0g1 = w0gauss ( enk / degaussw0, 0) / degaussw0
         w0g2 = w0gauss ( enpkp / degaussw0, 0) / degaussw0
-!       mu = mu + (1.0d0/N0)*vcnknpkp(ik,ikp,ibnd,jbnd)*w0g1*w0g2*wk(ik)*wk(ikp)
 !       Again we want per spin.
         mu = mu + (1.0d0/N0)*vcnknpkp(ik,ikp,ibnd,jbnd)*w0g1*w0g2*(wk(ik)/2.0)
      enddo
@@ -524,12 +547,13 @@ IMPLICIT NONE
    enddo
   enddo
 !CALL mp_sum(mu, inter_pool_comm)
-!for aluminum we take debye temperature is 428 K convert to rydberg.
-  debye_e = 0.036
 !Factors not included when we calculate V^{c}_{nkn'k'}.
   mu = mu/omega/nqs
+  print*, "Ef", ef*rytoev, "N(0)", N0/rytoev
+  print*, "debye temp Ry", debye_e
   print*, "\mu", mu
-  print*, "\mu^{*}", mu/(1+mu*log((12.43/RYTOEV)/debye_e))
+  print*, "\mu^{*}", mu/(1+mu*log((ef)/debye_e))
+
 END SUBROUTINE fsaverage
 
 REAL(DP) FUNCTION lind_eps(qg2)
@@ -537,38 +561,8 @@ REAL(DP) FUNCTION lind_eps(qg2)
   USE dielectric,  ONLY : qtf, kf
   IMPLICIT NONE
   REAL(DP) :: qg2, x
-!qtf tommy fermi wave vector for Al... (according to my calculations)
-
   x = qg2/(2*kf)
   lind_eps = 1.0d0 + (qtf)**2.0/(2.0*qg2**2)*(1.0d0+(1.0d0/(2.0*x))*(1-x**2)*log(abs((1+x)/(1-x))))
-  !lind_eps = 1.0d0 
   RETURN
 END FUNCTION
-!Construct screened coulomb interaction
-!rcut = (float(3)/float(4)/pi*omega*float(nq1*nq2*nq3))**(float(1)/float(3))
-!do ig = 1, ngmpol
-!  qg2 = (g(1,ig) + xq(1))**2 + (g(2,ig) + xq(2))**2 + (g(3,ig)+xq(3))**2
-!  limq = (qg2.lt.eps8) 
-!  if(.not.limq) then
-!     do igp = 1, ngmpol
-!        vc(ig, igp) = scrcoul(ig,igp,1)*dcmplx(e2*fpi/(tpiba2*qg2), 0.0d0)
-!     enddo
-!  endif
-!  qg = sqrt(qg2)
-!  spal = 1.0d0 - cos(rcut*sqrt(tpiba2)*qg)
-!  if(.not.limq) then
-!      do igp = 1, ngmpol
-!         vc = scrcoul(ig,igp,1)*dcmplx(spal, 0.0d0)
-!      enddo
-!  else
-!      scrcoul_g_R(ig,igp,1) = scrcoul_g_R(ig,igp,iw)*dcmplx((fpi*e2*(rcut**2))/2.0d0, 0.0d0)
-!  endif
-!enddo
-!         do ir = 1, dffts%nnr
-!            !fnknpkp(ir) = fnknpkp(ir) + conjg(psi_temp(ir))*psi_temp(ir)
-!             fnknpkp(ir) = fnknpkp(ir) + conjg(psink(ir,ibnd))*psink(ir,ibnd)
-!             norm   = norm + conjg(psink(ir,ibnd))*psink(ir,ibnd)/(dffts%nnr)
-!         enddo
-!         CALL fwfft ('Wave', fnknpkp(:), dffts)
-!         print*, sum(fnknpkp(nls(igk(1:npw)))), fnknpkp(1)
-          !print*, sum(fnknpkp(nls(igk(1:npw)))), fnknpkp(1)
+
