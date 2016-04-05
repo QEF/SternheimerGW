dce69f77f5043de35c3ed5c060061cd435c45a20
Martin Schlipf <martin.schlipf@gmail.com>
2016-04-05 17:41:55 +0100

    clean up sigma_matel
    add some comments to describe the code better
    consistent uppercase throughout the file

--- src/sigma_matel.f90
+++ src/sigma_matel.f90
@@ -30,18 +30,18 @@ SUBROUTINE sigma_matel (ik0)
   USE gvecs,                ONLY : nls
   USE constants,            ONLY : e2, fpi, RYTOEV, tpi, pi
   USE freq_gw,              ONLY : nwsigma, wsigma, wsig_wind_min, wsig_wind_max, deltaws, nwsigwin
-  USE klist,                ONLY : xk, nkstot
+  USE klist,                ONLY : xk
   USE wvfct,                ONLY : nbnd, npw, npwx, igk, g2kin
   USE gvecw,                ONLY : ecutwfc
   USE qpoint,               ONLY : npwq
   USE units_gw,             ONLY : iunsigma, iuwfc, lrwfc, lrsigma, lrsex, iunsex
-  USE control_gw,           ONLY : lgamma, do_imag, do_sigma_exxG !, tmp_dir_coul
+  USE control_gw,           ONLY : lgamma, do_imag, do_sigma_exxG
   USE wavefunctions_module, ONLY : evc
   USE gwsigma,              ONLY : sigma_x_st, sigma_c_st, nbnd_sig, corr_conv, exch_conv, &
                                    sigma_band_exg, gcutcorr
   USE disp,                 ONLY : xk_kpoints
   USE scf,                  ONLY : rho, rho_core, rhog_core, scf_type, v
-  USE cell_base,            ONLY : tpiba2, at, alat
+  USE cell_base,            ONLY : tpiba2
   USE buiol,                ONLY : buiol_check_unit
   USE fft_base,             ONLY : dffts, dfftp
   USE fft_interfaces,       ONLY : invfft, fwfft
@@ -53,87 +53,101 @@ SUBROUTINE sigma_matel (ik0)
   USE output_mod,           ONLY : filsigx, filsigc
 
 IMPLICIT NONE
-  complex(DP), allocatable  :: sigma_band_con(:,:,:)
-  complex(DP)               ::   czero
-  complex(DP)               ::   psic(dffts%nnr), vpsi(ngm)
-  complex(DP)               ::   ZdoTC, sigma_band_c(nbnd_sig, nbnd_sig, nwsigma),&
-                                 sigma_band_x(nbnd_sig, nbnd_sig, 1), vxc(nbnd_sig,nbnd_sig)
-  real(DP), allocatable     ::   wsigwin(:)
-  real(DP)                  ::   w_ryd(nwsigma)
-  real(DP)                  ::   one, zcut
-  real(DP)    :: vtxc, etxc
-  real(DP)    :: zero(3)
-  integer                   ::   ikq, ikq_head
-  integer                   ::   ig, iw, ibnd, jbnd, ipol, ik0, ir
-  integer                   ::   ng
-  integer     :: sigma_c_ngm, sigma_x_ngm
-  integer     :: kpoolid(nkstot), iqrec1(nkstot)
-  logical, external :: eqvect
-  logical :: found_k, exst, opnd
-  real(DP), parameter :: eps=1.e-5_dp
+
+  COMPLEX(DP), ALLOCATABLE  :: sigma_band_con(:,:,:)
+  COMPLEX(DP)               :: psic(dffts%nnr), vpsi(ngm)
+  COMPLEX(DP)               :: ZdoTC, sigma_band_c(nbnd_sig, nbnd_sig, nwsigma),&
+                               sigma_band_x(nbnd_sig, nbnd_sig, 1), vxc(nbnd_sig,nbnd_sig)
+  REAL(DP), ALLOCATABLE     :: wsigwin(:)
+  REAL(DP)                  :: vtxc, etxc
+  INTEGER                   :: ikq
+  INTEGER                   :: ig, iw, ibnd, jbnd, ipol, ik0, ir
+  INTEGER                   :: ng
+  INTEGER                   :: sigma_c_ngm, sigma_x_ngm
+  LOGICAL                   :: exst, opnd
 
   IF ( .NOT. meta_ionode ) RETURN
 
-  one   = 1.0d0 
-  czero = (0.0d0, 0.0d0)
-  w_ryd = wsigma(:nwsigma)/RYTOEV
-  nbnd = nbnd_sig 
-  zero(:) = 0.d0
-  lgamma=.true.
-  ikq = 1
-  found_k = .false.
+  nbnd   = nbnd_sig 
+  lgamma = .true.
 
-  if((xk_kpoints(1,ik0).eq.0.0).and.(xk_kpoints(2,ik0).eq.0.0).and.(xk_kpoints(3,ik0).eq.0.0))then 
-     ikq_head = 1
-  else
-     ikq_head = 2
-  endif
+  ! check for gamma point
+  IF( ALL(xk_kpoints(:,ik0) == 0.0) ) THEN
+     ikq = 1
+  ELSE
+     ikq = 2
+  END IF
 
-  ikq = ikq_head
-  write(stdout,'(/4x,"k0(",i3," ) = (", 3f7.3, " )")') ik0, (xk_kpoints(ipol,ik0) , ipol = 1, 3)
-  write(stdout,'(/4x,"k0(",i3," ) = (", 3f7.3, " )")') ikq, (xk(ipol,ikq) , ipol = 1, 3)
-  kpoolid = 0
-  iqrec1  = 0
+  WRITE(stdout,'(/4x,"k0(",i3," ) = (", 3f7.3, " )")') ik0, (xk_kpoints(ipol,ik0) , ipol = 1, 3)
+  WRITE(stdout,'(/4x,"k0(",i3," ) = (", 3f7.3, " )")') ikq, (xk(ipol,ikq) , ipol = 1, 3)
 
-  vxc(:,:) = czero
-  sigma_band_x = czero
-  sigma_band_c = czero
+  ! set matrix elements to 0
+  vxc          = 0
+  sigma_band_x = 0
+  sigma_band_c = 0
 
-  write(1000+mpime,'(/4x,"k0(",i3," ) = (", 3f7.3, " )")') ikq, (xk(ipol,ikq) , ipol = 1, 3)
+  WRITE(1000+mpime,'(/4x,"k0(",i3," ) = (", 3f7.3, " )")') ikq, (xk(ipol,ikq) , ipol = 1, 3)
+
+  ! create map to G ordering at current k-point
   CALL gk_sort( xk(1,ikq), ngm, g, ( ecutwfc / tpiba2 ),&
                 npw, igk, g2kin )
   npwq = npw
-  call get_buffer (evc, lrwfc, iuwfc, ikq)
-  zcut = 0.50d0*sqrt(at(1,3)**2 + at(2,3)**2 + at(3,3)**2)*alat
-! generate v_xc(r) in real space:
-  v%of_r(:,:) = (0.0d0)
-  CALL v_xc( rho, rho_core, rhog_core, etxc, vtxc, v%of_r )
-  write(1000+mpime, '("Taking Matels.")')
-  write(1000+mpime, '("Taking NPWQ.", i4)') npwq
-  write(1000+mpime, '("my_image_id", i4)') my_image_id
-  do jbnd = 1, nbnd_sig
-     psic = czero
-     do ig = 1, npwq
-        psic ( nls (igk(ig)) ) = evc(ig, jbnd)
-     enddo
-!Need to do this fft according to igkq arrays and switching between serial/parallel routines. 
-     CALL invfft ('Wave', psic(:), dffts)
-     do ir = 1, dfftp%nnr
-        psic (ir) = psic(ir) * v%of_r (ir,1)
-     enddo
-     CALL fwfft ('Wave', psic(:), dffts)
-     do ig = 1, npwq
-        vpsi(ig) = psic(nls(igk(ig)))
-     enddo
-     do ibnd = 1, nbnd_sig
-        vxc(ibnd,jbnd) = ZdoTC (npwq, evc (1, ibnd), 1, vpsi, 1)
-     enddo
-  enddo
-  write(1000+mpime, '(4x,"VXC (eV)")')
-  write(1000+mpime, '(8(1x,f7.3))') real(vxc(:,:))*RYTOEV
-  write(1000+mpime, '("Max number Plane Waves WFC ", i4)') npwx
-  write(1000+mpime, '("Sigma_Ex Matrix Element")') 
+  ! read wave functions at current k-point
+  CALL get_buffer (evc, lrwfc, iuwfc, ikq)
 
+  !
+  ! expectation value of V_xc
+  !
+
+  ! generate v_xc(r) in real space:
+  v%of_r = 0
+  CALL v_xc( rho, rho_core, rhog_core, etxc, vtxc, v%of_r )
+
+  WRITE(1000+mpime, '("Taking Matels.")')
+  WRITE(1000+mpime, '("Taking NPWQ.", i4)') npwq
+  WRITE(1000+mpime, '("my_image_id", i4)') my_image_id
+
+  ! loop over all bands
+  DO jbnd = 1, nbnd_sig
+
+    ! extract right wave function
+    psic = 0
+    DO ig = 1, npwq
+      ! map G-vectors according to smooth igk
+      psic ( nls (igk(ig)) ) = evc(ig, jbnd)
+    END DO
+
+    ! FFT wave function to real space
+    CALL invfft ('Wave', psic(:), dffts)
+
+    ! product of wave function and potential
+    DO ir = 1, dfftp%nnr
+      psic (ir) = psic(ir) * v%of_r (ir,1)
+    END DO
+
+    ! FFT back to reciprocal space
+    CALL fwfft ('Wave', psic(:), dffts)
+
+    ! reorder back to original order
+    DO ig = 1, npwq
+      vpsi(ig) = psic(nls(igk(ig)))
+    END DO
+
+    ! multiply with left wave function 
+    DO ibnd = 1, nbnd_sig
+      vxc(ibnd,jbnd) = ZdoTC (npwq, evc (1, ibnd), 1, vpsi, 1)
+    END DO
+
+  END DO ! bands
+
+  WRITE(1000+mpime, '(4x,"VXC (eV)")')
+  WRITE(1000+mpime, '(8(1x,f7.3))') real(vxc(:,:))*RYTOEV
+  WRITE(1000+mpime, '("Max number Plane Waves WFC ", i4)') npwx
+  WRITE(1000+mpime, '("Sigma_Ex Matrix Element")') 
+
+  !
+  ! expectation value of Sigma_x
+  !
   IF( .NOT. do_sigma_exxG) THEN
 
     ! open file containing exchange part of sigma
@@ -167,7 +181,9 @@ IMPLICIT NONE
 
   END IF
 
-!MATRIX ELEMENTS OF SIGMA_C:
+  !
+  ! expectation value of Sigma_c:
+  !
   WRITE(1000+mpime,*) 
   WRITE(1000+mpime, '("sigma_c matrix element")') 
 
@@ -175,8 +191,8 @@ IMPLICIT NONE
   INQUIRE( UNIT=iunsigma, OPENED=opnd )
   IF (.NOT. opnd) CALL diropn( iunsigma, filsigc, lrsigma, exst )
 
-!For convergence tests corr_conv can be set at input lower than ecutsco.
-!This allows you to calculate the correlation energy at lower energy cutoffs
+  ! For convergence tests corr_conv can be set at input lower than ecutsco.
+  ! This allows you to calculate the correlation energy at lower energy cutoffs
   IF (corr_conv == sigma_c_st%ecutt) THEN
     sigma_c_ngm = gcutcorr
   ELSE IF(corr_conv < sigma_c_st%ecutt .AND. corr_conv > 0.0) THEN
@@ -196,23 +212,27 @@ IMPLICIT NONE
   CALL sigma_expect_file(iunsigma,ik0,evc,sigma_c_ngm,igk,sigma_band_c,nwsigma)
   WRITE (1000+mpime,'("Finished Sigma_c")')
 
-  if(do_imag) then 
-!We can set arbitrary \Sigma(\omega) energy windows with analytic continuation:
-    allocate (wsigwin(nwsigwin))
-    do iw = 1, nwsigwin
+  !
+  ! analytic continuation from imaginary frequencies to real ones
+  ! 
+  IF (do_imag) THEN
+    ! We can set arbitrary \Sigma(\omega) energy windows with analytic continuation:
+    ALLOCATE (wsigwin(nwsigwin))
+    DO iw = 1, nwsigwin
         wsigwin(iw) = wsig_wind_min + (wsig_wind_max-wsig_wind_min)/float(nwsigwin-1)*float(iw-1)
-    enddo
-    allocate (sigma_band_con(nbnd_sig, nbnd_sig, nwsigwin))
-!print selfenergy on the imaginary axis.
-    call print_matel_im(ikq_head, vxc(1,1), sigma_band_x(1,1,1), sigma_band_c(1,1,1), wsigma(1), nwsigma)
-!do analytic continuation and print selfenergy on the real axis.
+    END DO
+    ALLOCATE (sigma_band_con(nbnd_sig, nbnd_sig, nwsigwin))
+    ! print selfenergy on the imaginary axis.
+    CALL print_matel_im(ikq, vxc(1,1), sigma_band_x(1,1,1), sigma_band_c(1,1,1), wsigma(1), nwsigma)
+    ! do analytic continuation and print selfenergy on the real axis.
     sigma_band_con(:,:,:) = dcmplx(0.0d0, 0.d0)
-    call sigma_pade(sigma_band_c(1,1,1), sigma_band_con(1,1,1), wsigwin(1), nwsigwin)
-    call print_matel(ikq_head, vxc(1,1), sigma_band_x(1,1,1), sigma_band_con(1,1,1), wsigwin(1), nwsigwin)
-  else
-    call print_matel(ikq_head, vxc(1,1), sigma_band_x(1,1,1), sigma_band_c(1,1,1), wsigma(1), nwsigma)
-  endif
-  if(allocated(sigma_band_con)) deallocate(sigma_band_con)
-  if(allocated(sigma_band_exg)) deallocate(sigma_band_exg)
+    CALL sigma_pade(sigma_band_c(1,1,1), sigma_band_con(1,1,1), wsigwin(1), nwsigwin)
+    CALL print_matel(ikq, vxc(1,1), sigma_band_x(1,1,1), sigma_band_con(1,1,1), wsigwin(1), nwsigwin)
+   deallocate(sigma_band_con)
+  ELSE
+    ! print sigma on real axis
+    CALL print_matel(ikq, vxc(1,1), sigma_band_x(1,1,1), sigma_band_c(1,1,1), wsigma(1), nwsigma)
+  END IF
+  IF (allocated(sigma_band_exg)) DEALLOCATE(sigma_band_exg)
 
-end SUBROUTINE sigma_matel
+END SUBROUTINE sigma_matel
