7650f9e398845d870e0220bef3b4c947fbdf6265
Martin Schlipf <martin.schlipf@gmail.com>
2016-05-19 11:46:44 +0000

    create interface to print the data
    in pp_output, we have an interface that links to functions printing a data
    array in a format that can be read by plotband of QE
    so far, we created dummy routines for 2d or 3d data structure
    the last array dimension must be the number of k-points
    
    git-svn-id: https://rothery3.materials.ox.ac.uk/svn/sgw/branches/sgw-5.0@832 21dd5d8e-1f79-4369-87ad-170e0d29e828

--- src/pp_output.f90
+++ src/pp_output.f90
@@ -23,6 +23,42 @@
 !> Provides routines that generate output for QE's plotband program
 MODULE pp_output_mod
 
+  USE kinds, ONLY : dp
+
+  IMPLICIT NONE
+
+  !> Print the an array for QE's plotband program
+  !!
+  !! The array has the shape (a1, ..., an, b), where a1 to an are
+  !! multiplied to form the nbnd flag for plotband. b is the number
+  !! of k-points of the system.
+  !! \param filename file to which the data is printed
+  !! \param kpt array containing the k-points
+  !! \param data array containing the data
+  INTERFACE pp_output
+    MODULE PROCEDURE pp_output_2d, pp_output_3d
+  END INTERFACE pp_output
+
+  PRIVATE pp_output_2d, pp_output_3d
+
 CONTAINS
+
+  !> specialization of the interface for 2d data
+  SUBROUTINE pp_output_2d(filename, kpt, data)
+
+    CHARACTER(LEN=*), INTENT(IN) :: filename
+    REAL(dp),         INTENT(IN) :: kpt(:,:)
+    REAL(dp),         INTENT(IN) :: data(:,:)
+
+  END SUBROUTINE pp_output_2d
+
+  !> specialization of the interface for 3d data
+  SUBROUTINE pp_output_3d(filename, kpt, data)
+
+    CHARACTER(LEN=*), INTENT(IN) :: filename
+    REAL(dp),         INTENT(IN) :: kpt(:,:)
+    REAL(dp),         INTENT(IN) :: data(:,:,:)
+
+  END SUBROUTINE pp_output_3d
 
 END MODULE pp_output_mod
