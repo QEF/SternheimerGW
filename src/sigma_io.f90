4d0d7f75638d2b643e48e5f42d9074226c93c435
Martin Schlipf <martin.schlipf@gmail.com>
2016-05-27 12:51:19 +0100

    implement the write of sigma
    write kpt, exchange and correlation part of sigma

--- src/sigma_io.f90
+++ src/sigma_io.f90
@@ -31,10 +31,53 @@ MODULE sigma_io_module
 
   IMPLICIT NONE
 
+  !> xml tag for the sigma matrix
+  CHARACTER(*), PARAMETER :: tag_sigma = "SIGMA"
+
+  !> xml tag for the k-point
+  CHARACTER(*), PARAMETER :: tag_kpt = "KPOINT"
+
+  !> xml tag for the exchange part
+  CHARACTER(*), PARAMETER :: tag_exchange = "EXCHANGE"
+
+  !> xml tag for the correlation part
+  CHARACTER(*), PARAMETER :: tag_correlation = "CORRELATION"
+
 CONTAINS
 
-  !> Write Sigma to disk
-  SUBROUTINE sigma_io_write
+  !> Write Sigma to disk.
+  !!
+  !! Write the self-energy for exchange and correlation separately.
+  !!
+  !! \param[in] iunit Unit to which the Sigma is written, must be opened with iotk_module.
+  !! \param[in] ikpt Index of the k-point.
+  !! \param[in] sigma_x Exchange part of Sigma.
+  !! \param[in] sigma_c Correlation part of Sigma (frequency dependent).
+  !!
+  SUBROUTINE sigma_io_write(iunit, ikpt, sigma_x, sigma_c)
+
+    USE iotk_module, ONLY: iotk_index, iotk_namlenx, &
+                           iotk_write_begin, iotk_write_end, iotk_write_dat
+
+    INTEGER,     INTENT(IN) :: iunit
+    INTEGER,     INTENT(IN) :: ikpt
+    COMPLEX(dp), INTENT(IN) :: sigma_x(:,:)
+    COMPLEX(dp), INTENT(IN) :: sigma_c(:,:,:)
+
+    CHARACTER(LEN=iotk_namlenx) tag_sigma_loc
+
+    tag_sigma_loc = tag_sigma // iotk_index(iunit)
+
+    CALL iotk_write_begin(iunit, tag_sigma_loc)
+
+    ! write the exchange part
+    CALL iotk_write_dat(iunit, tag_exchange, sigma_x)
+
+    ! write the correlation part
+    CALL iotk_write_dat(iunit, tag_correlation, sigma_c)
+
+    CALL iotk_write_end(iunit, tag_sigma_loc)
+
   END SUBROUTINE sigma_io_write
 
   !> Read Sigma from disk
