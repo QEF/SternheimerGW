a3feda66def6efa23b93a093c31c2df2f59eb029
Martin Schlipf <martin.schlipf@gmail.com>
2016-06-13 17:00:19 +0100

    Use git instead of svn to determine version number
    Replace svn commands in update_version script with more descriptive git ones.
    Modify opening message to account for git instead of svn.
    
    Longer update to documentation of version.f90.in to account for the idea, how
    to define the tags and the version numbers.

--- src/sgw_opening.f90
+++ src/sgw_opening.f90
@@ -20,21 +20,24 @@
 ! http://www.gnu.org/licenses/gpl.html .
 !
 !------------------------------------------------------------------------------ 
+!> Write the opening message.
+!!
+!! Print the version number used and the git revision if known.
+!------------------------------------------------------------------------------
 SUBROUTINE sgw_opening_message()
 
-    USE kinds, ONLY: DP
-    USE io_global,  ONLY :  stdout, ionode, ionode_id
-    USE sgw_version, ONLY: sgw_version_number, sgw_svn_revision
+    USE io_global,   ONLY: stdout
+    USE sgw_version, ONLY: sgw_version_number, sgw_git_describe
 
     IMPLICIT NONE
 
-    WRITE( stdout, '(/5X, "You are using SGW version: ", A)') sgw_version_number
-    WRITE( stdout, '(/5X, "With svn revision number: ", A)') sgw_svn_revision
+    WRITE(stdout, '(/5X, "You are using SGW version: ", A)') sgw_version_number
+    WRITE(stdout, '(/5X, "Git description of commit: ", A)') sgw_git_describe
 
-    WRITE( stdout, '(/5X,"This program is part of the open-source Quantum ", &
+    WRITE(stdout, '(/5X,"This program is part of the open-source Quantum ", &
          &  "ESPRESSO suite", &
          &/9X," Please also cite H. Lambert and F. Giustino, Phys. Rev. B ",&
          &    " 88. 075117 (2013);", &
          &/9X," URL http://www.sternheimer.org.uk")' )
-    RETURN
+
 END SUBROUTINE sgw_opening_message
