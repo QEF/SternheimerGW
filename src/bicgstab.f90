265aa9228fa8cd90c4c0a3126c8884dc61595a61
Martin Schlipf <martin.schlipf@gmail.com>
2016-07-05 11:21:56 +0100

    initialize the variables
    there are also some variable quantities that we need to track for Fromme's
    algorithm which are stored in the seed and shift system

--- src/bicgstab.f90
+++ src/bicgstab.f90
@@ -20,11 +20,11 @@
 ! http://www.gnu.org/licenses/gpl.html .
 !
 !------------------------------------------------------------------------------
-!> Shifted BiCGStab(l) solver for linear equation \f$(A + \lambda I) x = b\f$.
+!> Shifted BiCGStab(l) solver for linear equation \f$(A + \sigma I) x = b\f$.
 !!
 !! Implements the shifted BiCGStab(l) algorithm according to the paper of
 !! Fromme, Computing **70**, 87 (2003). The general idea is that the matrix
-!! \f$A\f$ and \f$A + \lambda I\f$ span the same Krylov subspace. Hence, we can
+!! \f$A\f$ and \f$A + \sigma I\f$ span the same Krylov subspace. Hence, we can
 !! solve the linear equation of all linear problems at the cost of a single one.
 !!
 MODULE bicgstab_module
@@ -49,6 +49,18 @@ MODULE bicgstab_module
     !> residual vector
     COMPLEX(dp), ALLOCATABLE :: rr(:)
 
+    !> \f$\rho_{\text{old}}\f$ of Fromme's algorithm
+    COMPLEX(dp) rho_old
+
+    !> \f$\alpha_{\text{old}}\f$ of Fromme's algorithm
+    COMPLEX(dp) alpha_old
+
+    !> \f$\alpha\f$ of Fromme's algorithm
+    COMPLEX(dp) alpha
+
+    !> \f$\omega\f$ of Fromme's algorithm
+    COMPLEX(dp) omega
+  
   END TYPE seed_system_type
 
   !> Variables and arrays used for the shifted system.
@@ -60,6 +72,18 @@ MODULE bicgstab_module
     !> approximate solution to the linear system
     COMPLEX(dp), ALLOCATABLE :: xx(:)
 
+    !> \f$\phi_{\text{old}}^\sigma\f$ of Fromme's algorithm
+    COMPLEX(dp) phi_old
+
+    !> \f$\phi^\sigma\f$ of Fromme's algorithm
+    COMPLEX(dp) phi
+
+    !> \f$\vartheta^\sigma\f$ of Fromme's algorithm
+    COMPLEX(dp) theta
+
+    !> \f$\mu_{ij}\f$ of Fromme's algorithm
+    COMPLEX(dp), ALLOCATABLE :: mu(:)
+
   END TYPE shift_system_type
 
 CONTAINS
@@ -68,13 +92,16 @@ CONTAINS
   !!
   !! This subroutine implements the *Algorithm 2* of Fromme's paper.
   !!
-  SUBROUTINE bicgstab(lmax, bb)
+  SUBROUTINE bicgstab(lmax, bb, sigma)
 
     !> Dimensionality of the GMRES algorithm.
     INTEGER,     INTENT(IN) :: lmax
 
     !> Right hand side of the linear equation.
     COMPLEX(dp), INTENT(IN) :: bb(:)
+
+    !> Shifts \f$\sigma\f$ relative to the seed system.
+    COMPLEX(dp), INTENT(IN) :: sigma(:)
 
     !> Size of the right-hand vector.
     INTEGER vec_size
@@ -91,7 +118,7 @@ CONTAINS
 
     !> Contains the current best guess for the solution of the linear
     !! system and the corresponding residual in the shifted system.
-    TYPE(shift_system_type) shift_system(1)
+    TYPE(shift_system_type) shift_system(SIZE(sigma))
 
     ! determine dimension of vector
     vec_size = SIZE(bb)
@@ -104,7 +131,7 @@ CONTAINS
     !
     ! initialization shifted systems
     !
-    CALL init_shift(vec_size, shift_system)
+    CALL init_shift(lmax, vec_size, sigma, shift_system)
 
     ! loop until solution is found
     DO iter = 1, max_iter
@@ -160,6 +187,12 @@ CONTAINS
     ! initial residual is r = (b - A x) = b, because initial x = 0
     seed_system%rr = bb
 
+    ! init the variables
+    seed_system%rho_old   = 1.0
+    seed_system%alpha_old = 1.0
+    seed_system%alpha     = 0.0
+    seed_system%omega     = 1.0
+
   END SUBROUTINE init_seed
 
   !> Deallocate the arrays in the seed system type.
@@ -176,29 +209,120 @@ CONTAINS
   END SUBROUTINE destroy_seed
 
   !> Initialize the shifted systems.
-  SUBROUTINE init_shift(vec_size, shift_system)
+  SUBROUTINE init_shift(lmax, vec_size, sigma, shift_system)
+
+    !> Dimensionality of the GMRES algorithm.
+    INTEGER,     INTENT(IN) :: lmax
 
     !> Size of the vectors to initialize.
     INTEGER, INTENT(IN) :: vec_size
 
+    !> Shifts \f$\sigma\f$ relative to the seed system.
+    COMPLEX(dp), INTENT(IN) :: sigma(:)
+
     !> On output contains the initialized shifted systems.
-    TYPE(shift_system_type), INTENT(OUT) :: shift_system(:)
+    TYPE(shift_system_type), INTENT(OUT) :: shift_system(SIZE(sigma))
 
     !> counter over shifted systems
     INTEGER ishift
 
+    !> dimension of the array mu
+    INTEGER mu_size
+
+    !> binomials \f$\begin{pmatrix}j - 1\\ i - 1\end{pmatrix}\f$
+    REAL(dp),    ALLOCATABLE :: binomial(:)
+
+    !> array to store \f$\sigma^n\f$ for \f$n = 0, \ldots, l_{\text{max}}\f$
+    COMPLEX(dp), ALLOCATABLE :: sigma_pow(:)
+
+    !> counter variables to evaluate mu
+    INTEGER ii, jj, offset, ij
+
+    ! determine size of array mu
+    mu_size = lmax * (lmax + 1) / 2
+
+    ! allocate helper arrays
+    ALLOCATE(binomial(mu_size))
+    ALLOCATE(sigma_pow(0:lmax - 1))
+
+    !                     / j - 1 \        (j - 1)!
+    ! evaluate binomials (         ) = -----------------
+    !                     \ i - 1 /    (i - 1)! (j - i)!
+    ! note: shift by one compared to Fromme, because Fortran starts indices at 1
+    DO jj = 1, lmax
+
+      ! the ij index starts at this value
+      offset = jj * (jj - 1) / 2
+
+      DO ii = 1, jj
+
+        ! evaluate index in array
+        ij = offset + ii
+
+        ! trivial case i = 1: binomial = 1
+        IF (ii == 1) THEN
+          binomial(ij) = 1
+
+        ! in general, the following is valid
+        !  / j - 1 \     / j - 1 \  j - i + 1
+        ! (         ) = (         ) ---------
+        !  \ i - 1 /     \ i - 2 /    i - 1
+        ELSE
+          binomial(ij) = binomial(ij - 1) * (jj - ii + 1) / (ii - 1)
+
+        END IF
+
+      END DO ! i
+    END DO ! j
+
     ! loop over all shifted systems
-    DO ishift = 1, SIZE(shift_system)
+    DO ishift = 1, SIZE(sigma)
 
       ! allocate arrays of given size
       ALLOCATE(shift_system(ishift)%uu(vec_size))
       ALLOCATE(shift_system(ishift)%xx(vec_size))
 
+      ! allocate array for mu
+      ALLOCATE(shift_system(ishift)%mu(mu_size))
+
       ! initialize arrays to 0
       shift_system(ishift)%uu = 0
       shift_system(ishift)%xx = 0
 
+      ! initialize the variables
+      shift_system(ishift)%phi_old = 1.0
+      shift_system(ishift)%phi     = 1.0
+      shift_system(ishift)%theta   = 1.0
+
+      ! construct sigma_pow array
+      sigma_pow(0) = 1.0
+      DO ii = 1, lmax - 1
+        sigma_pow(ii) = sigma(ishift) * sigma_pow(ii - 1)
+      END DO ! i
+
+      ! construct mu_ij
+      DO jj = 1, lmax
+
+        ! the ij index starts at this value
+        offset = jj * (jj - 1) / 2
+
+        DO ii = 1, jj
+
+          ! evaluate index in array
+          ij = offset + ii
+
+          !          /j - 1\       j - i
+          ! mu_ij = (       ) sigma
+          !          \i - 1/
+          shift_system(ishift)%mu(ij) = binomial(ij) * sigma_pow(jj - ii)
+
+        END DO ! i
+      END DO ! j
+
     END DO ! ishift
+
+    DEALLOCATE(binomial)
+    DEALLOCATE(sigma_pow)
 
   END SUBROUTINE init_shift
 
@@ -217,6 +341,7 @@ CONTAINS
       ! deallocate arrays
       DEALLOCATE(shift_system(ishift)%uu)
       DEALLOCATE(shift_system(ishift)%xx)
+      DEALLOCATE(shift_system(ishift)%mu)
 
     END DO ! ishift
 
