b8e8e962a91c1e9285129239d31539f578bd9b3f
Martin Schlipf <martin.schlipf@gmail.com>
2016-08-08 10:30:16 +0100

    code style improvements in freqbins.f90

--- src/freqbins.f90
+++ src/freqbins.f90
@@ -87,131 +87,130 @@ CONTAINS
   !      course window for everything outside this range.
   SUBROUTINE freqbins()
  
-  USE freq_gw,    ONLY : nwcoul, nwgreen, nwsigma, wcoul,& 
-                         wgreen, wsigma, wsigmamin, wsigmamax,&
-                         deltaw, wcoulmax, &
-                         fiu, nfs, w0pmw, wgtcoul, &
-                         wsig_wind_max, wsig_wind_min, deltaws, nwsigwin
-  USE io_global,  ONLY :  stdout, ionode, ionode_id
-  USE kinds,      ONLY : DP
-  USE constants,  ONLY : RYTOEV, pi
-  USE control_gw, ONLY : eta, godbyneeds, padecont, do_imag
-  USE disp,       ONLY : num_k_pts, w_of_k_start, w_of_k_stop, xk_kpoints,& 
-                         nq1, nq2, nq3
-  USE cell_base,  ONLY : omega, tpiba2, at, bg, tpiba, alat
-           
+    USE freq_gw,    ONLY : nwcoul, nwgreen, nwsigma, wcoul,& 
+                           wgreen, wsigma, wsigmamin, wsigmamax,&
+                           deltaw, wcoulmax, &
+                           fiu, nfs, w0pmw, wgtcoul, &
+                           wsig_wind_max, wsig_wind_min, deltaws, nwsigwin
+    USE io_global,  ONLY :  stdout, ionode, ionode_id
+    USE kinds,      ONLY : DP
+    USE constants,  ONLY : RYTOEV, pi
+    USE control_gw, ONLY : eta, godbyneeds, padecont, do_imag
+    USE disp,       ONLY : num_k_pts, w_of_k_start, w_of_k_stop, xk_kpoints,& 
+                           nq1, nq2, nq3
+    USE cell_base,  ONLY : omega, tpiba2, at, bg, tpiba, alat
 
-  IMPLICIT NONE 
+    IMPLICIT NONE 
 
-  REAL(DP)  :: zero, w0mw, w0pw, rcut
-  REAL(DP)  :: x1, x2       !ranges
-  REAL(DP), ALLOCATABLE  :: x(:), w(:)   !abcissa and weights
-  INTEGER  :: n            !number of points
-  INTEGER  :: nwalloc
-  INTEGER  :: iw, iw0, iwp, iw0mw, iw0pw, i
-  LOGICAL  :: foundp, foundm
+    REAL(DP)  :: zero, w0mw, w0pw, rcut
+    REAL(DP)  :: x1, x2       !ranges
+    REAL(DP), ALLOCATABLE  :: x(:), w(:)   !abcissa and weights
+    INTEGER  :: n            !number of points
+    INTEGER  :: nwalloc
+    INTEGER  :: iw, iw0, iwp, iw0mw, iw0pw, i
+    LOGICAL  :: foundp, foundm
 
-  REAL(dp) :: wgreenmin, wgreenmax
-  REAL(dp), ALLOCATABLE :: wtmp(:)
+    REAL(dp) :: wgreenmin, wgreenmax
+    REAL(dp), ALLOCATABLE :: wtmp(:)
 
-  zero = 0.0d0
-  IF (.NOT.do_imag) THEN
-   !wgreenmin = wsigmamin-wcoulmax
-   !wgreenmax = wsigmamax+wcoulmax
-!for change of variables in Green's function
-   wgreenmin = -wcoulmax
-   wgreenmax = wcoulmax
+    zero = 0.0d0
+    IF (.NOT.do_imag) THEN
+      !wgreenmin = wsigmamin-wcoulmax
+      !wgreenmax = wsigmamax+wcoulmax
+      !for change of variables in Green's function
+      wgreenmin = -wcoulmax
+      wgreenmax = wcoulmax
 
-   nwalloc = 1 + ceiling( (wgreenmax-wgreenmin) / deltaw )
-   allocate(wtmp(nwalloc), wcoul(nwalloc), wgreen(nwalloc), wsigma(nwalloc) )
-   wcoul = zero
-   wgreen = zero
-   wsigma = zero
-   do iw = 1, nwalloc
-      wtmp(iw) = wgreenmin + (wgreenmax-wgreenmin)/float(nwalloc-1)*float(iw-1)
-   enddo
-   nwgreen = 0
-   nwcoul = 0
-   nwsigma = 0
-   do iw = 1, nwalloc
-    if ( ( wtmp(iw) .ge. zero ) .and. ( wtmp(iw) .le. wcoulmax) ) then
-      nwcoul = nwcoul + 1
-      wcoul(nwcoul) = wtmp(iw)
-    endif
-    if ( ( wtmp(iw) .ge. wsigmamin ) .and. ( wtmp(iw) .le. wsigmamax) ) then
-      nwsigma = nwsigma + 1
-      wsigma(nwsigma) = wtmp(iw)
-    endif
-   enddo
+      nwalloc = 1 + ceiling( (wgreenmax-wgreenmin) / deltaw )
+      ALLOCATE(wtmp(nwalloc), wcoul(nwalloc), wgreen(nwalloc), wsigma(nwalloc) )
+      wcoul = zero
+      wgreen = zero
+      wsigma = zero
+      DO iw = 1, nwalloc
+         wtmp(iw) = wgreenmin + (wgreenmax-wgreenmin)/float(nwalloc-1)*float(iw-1)
+      END DO
+      nwgreen = 0
+      nwcoul = 0
+      nwsigma = 0
+      DO iw = 1, nwalloc
+        IF ((wtmp(iw) >= zero) .AND. (wtmp(iw) <= wcoulmax)) THEN
+          nwcoul = nwcoul + 1
+          wcoul(nwcoul) = wtmp(iw)
+        END IF
+        IF ((wtmp(iw) >= wsigmamin) .AND. (wtmp(iw) <= wsigmamax)) THEN
+          nwsigma = nwsigma + 1
+          wsigma(nwsigma) = wtmp(iw)
+        END IF
+      END DO
 
-   nwgreen = 2*nwcoul
-   allocate ( w0pmw (nwsigma, 2*nwcoul) )
-   DO iw0 = 1, 1
-     DO iw = 1, nwcoul
-        w0pmw(iw0, iw)        = wsigma(iw0) + wcoul(iw)
-        w0pmw(iw0, iw+nwcoul) = wsigma(iw0) - wcoul(iw)
-     ENDDO
-   ENDDO
-  ELSE
-! We generate Sigma on a uniform grid:
-   nwgreen = 2*nwcoul
-   nwsigma = 1 + ceiling( (wsigmamax-wsigmamin) / deltaw )
-   allocate(wsigma(nwsigma))
-   allocate(wcoul(nwcoul), wgtcoul(nwcoul))
-   wcoul   = zero
-   wgtcoul = zero
-   wsigma  = zero
-   do iw = 1, nwsigma
-      wsigma(iw) = wsigmamin + (wsigmamax-wsigmamin)/float(nwsigma-1)*float(iw-1)
-   enddo
-! We generate W(iw) on a gauss legendre grid:
-   wcoul   = zero
-   wgtcoul = zero
-   CALL gauleg_grid(0.0d0, wcoulmax, wcoul, wgtcoul, nwcoul)
-! Correpspondence arrays for convolution arguments for green's function.
-   allocate ( w0pmw (nwsigma, 2*nwcoul) )
-   DO iw0 = 1, nwsigma
-     DO iw = 1, nwcoul
-        w0pmw(iw0, iw)        = wsigma(iw0) + wcoul(iw)
-        w0pmw(iw0, iw+nwcoul)        = wsigma(iw0) - wcoul(iw)
-     ENDDO
-   ENDDO
-   WRITE(stdout, '(7x, "Gauss-Legendre grid: ")')
-   DO i = 1, nwcoul
-      WRITE(stdout,'(8x, i4, 4x, 2f12.3)') i, wgtcoul(i), wcoul(i)
-   ENDDO
-  ENDIF
-  nwsigwin  = 1 + ceiling((wsig_wind_max - wsig_wind_min)/deltaws)
-! Print out Frequencies on Imaginary Axis for reference.
-  WRITE(stdout, '(//5x,"Frequency Grids (eV):")')
-  WRITE(stdout, '(/5x, "wsigmamin, wsigmamax, deltaw")')
-  WRITE(stdout, '(5x, 3f10.4 )') wsigmamin, wsigmamax, deltaw 
-  WRITE(stdout, '(/5x, "wcoulmax:", 1f10.4, " eV")') wcoulmax
-  WRITE(stdout, '(5x, "nwgreen:", i5)') nwgreen
-  WRITE(stdout, '(5x, "nwcoul:", i5)') nwcoul
-  WRITE(stdout,'(//5x, "Dynamic Screening Model:")')
-  IF(godbyneeds) then
+      nwgreen = 2*nwcoul
+      ALLOCATE(w0pmw(nwsigma, 2*nwcoul))
+      DO iw0 = 1, 1
+        DO iw = 1, nwcoul
+          w0pmw(iw0, iw)        = wsigma(iw0) + wcoul(iw)
+          w0pmw(iw0, iw+nwcoul) = wsigma(iw0) - wcoul(iw)
+        END DO
+      END DO
+    ELSE
+      ! We generate Sigma on a uniform grid:
+      nwgreen = 2*nwcoul
+      nwsigma = 1 + ceiling( (wsigmamax-wsigmamin) / deltaw )
+      ALLOCATE(wsigma(nwsigma))
+      ALLOCATE(wcoul(nwcoul), wgtcoul(nwcoul))
+      wcoul   = zero
+      wgtcoul = zero
+      wsigma  = zero
+      DO iw = 1, nwsigma
+        wsigma(iw) = wsigmamin + (wsigmamax-wsigmamin)/float(nwsigma-1)*float(iw-1)
+      END DO
+      ! We generate W(iw) on a gauss legendre grid:
+      wcoul   = zero
+      wgtcoul = zero
+      CALL gauleg_grid(0.0d0, wcoulmax, wcoul, wgtcoul, nwcoul)
+      ! Correpspondence arrays for convolution arguments for green's function.
+      ALLOCATE(w0pmw(nwsigma, 2*nwcoul))
+      DO iw0 = 1, nwsigma
+        DO iw = 1, nwcoul
+          w0pmw(iw0, iw)        = wsigma(iw0) + wcoul(iw)
+          w0pmw(iw0, iw+nwcoul) = wsigma(iw0) - wcoul(iw)
+        END DO
+      END DO
+      WRITE(stdout, '(7x, "Gauss-Legendre grid: ")')
+      DO i = 1, nwcoul
+        WRITE(stdout,'(8x, i4, 4x, 2f12.3)') i, wgtcoul(i), wcoul(i)
+      ENDDO
+    ENDIF
+    nwsigwin  = 1 + ceiling((wsig_wind_max - wsig_wind_min)/deltaws)
+    ! Print out Frequencies on Imaginary Axis for reference.
+    WRITE(stdout, '(//5x,"Frequency Grids (eV):")')
+    WRITE(stdout, '(/5x, "wsigmamin, wsigmamax, deltaw")')
+    WRITE(stdout, '(5x, 3f10.4 )') wsigmamin, wsigmamax, deltaw 
+    WRITE(stdout, '(/5x, "wcoulmax:", 1f10.4, " eV")') wcoulmax
+    WRITE(stdout, '(5x, "nwgreen:", i5)') nwgreen
+    WRITE(stdout, '(5x, "nwcoul:", i5)') nwcoul
+    WRITE(stdout,'(//5x, "Dynamic Screening Model:")')
+    IF(godbyneeds) THEN
       WRITE(stdout, '(/6x, "Godby Needs Plasmon-Pole")')
-  else if (padecont) then
+    ELSE IF (padecont) THEN
       WRITE(stdout, '(/6x, "Analytic Continuation")')
-  else if (.not.padecont.and..not.godbyneeds) then
+    ELSE IF (.NOT.padecont .AND. .NOT.godbyneeds) THEN
       WRITE(stdout, '(/6x, "No screening model chosen!")')
-  ENDIF
-  WRITE(stdout, '(/7x, "Imag. Frequencies: ")')
-  DO i = 1, nfs
-       WRITE(stdout,'(8x, i4, 4x, 2f9.4)')i, fiu(i)*RYTOEV
-  ENDDO
-  WRITE(stdout, '(/5x, "Broadening: ", 1f10.4)') eta
+    END IF
+    WRITE(stdout, '(/7x, "Imag. Frequencies: ")')
+    DO i = 1, nfs
+      WRITE(stdout,'(8x, i4, 4x, 2f9.4)')i, fiu(i)*RYTOEV
+    END DO
+    WRITE(stdout, '(/5x, "Broadening: ", 1f10.4)') eta
 
-  rcut = (float(3)/float(4)/pi*omega*float(nq1*nq2*nq3))**(float(1)/float(3))
-  WRITE(stdout, '(/5x, "Spherical Cutoff: ", 1f10.4)') rcut
+    rcut = (float(3)/float(4)/pi*omega*float(nq1*nq2*nq3))**(float(1)/float(3))
+    WRITE(stdout, '(/5x, "Spherical Cutoff: ", 1f10.4)') rcut
 
-  WRITE(stdout, '(/7x, "K-points: ", i4)') num_k_pts
-  DO i = 1, num_k_pts
-       WRITE(stdout,'(8x, i4, 4x, 3f9.4)')i, xk_kpoints(:, i)
-  ENDDO
+    WRITE(stdout, '(/7x, "K-points: ", i4)') num_k_pts
+    DO i = 1, num_k_pts
+      WRITE(stdout,'(8x, i4, 4x, 3f9.4)') i, xk_kpoints(:, i)
+    END DO
 
-END SUBROUTINE freqbins
+  END SUBROUTINE freqbins
 
   !> extract the number of frequencies in coarse mesh for W
   INTEGER FUNCTION freqbins_num_freq(this) RESULT (num_freq)
