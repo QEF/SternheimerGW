45d301b69e614da9f92f7d31f25815bd25faace0
Martin Schlipf <martin.schlipf@gmail.com>
2016-08-09 14:58:57 +0100

    implement calculation of Sigma correlation
    we reimplement a large part of the functionality of sigcim.f90
    
    For a given set of frequencies, we evaluate the Green's function and Fourier
    transform it to real space. Then we construct W at the necessary frequencies
    to convolute G and W to obtain Sigma.
    
    The differences of the new implementation are:
    - the new BiCGstab solver is used for the Green's function
    - the Fourier transform of G is only evaluated once instead of inside the loop
    - a better documentation and no dependence on external modules
    
    We have already included the code to treat real frequencies though it is
    not tested yet and hence commented out.

--- src/sigma.f90
+++ src/sigma.f90
@@ -97,7 +97,7 @@ MODULE sigma_module
 
   PRIVATE
 
-  PUBLIC sigma_real, sigma_imag
+  PUBLIC sigma_real, sigma_imag, sigma_correlation
 
 CONTAINS
 
@@ -191,5 +191,210 @@ CONTAINS
     CALL stop_clock(time_GW_product)
 
   END SUBROUTINE sigma_prod
+
+  !> Evaluate the correlation self-energy for a given frequency range.
+  !!
+  !! The algorithm consists of the following steps:
+  SUBROUTINE sigma_correlation(omega, fft_cust, multishift, lmax, threshold,  &
+                               mu, alpha, kpt, freq, first_sigma, &!eval, evec, &
+                               coulomb, sigma)
+
+    USE fft_custom,      ONLY: fft_cus
+    USE fft6_module,     ONLY: invfft6
+    USE freqbins_module, ONLY: freqbins_type
+    USE green_module,    ONLY: green_prepare, green_function
+    USE kinds,           ONLY: dp
+    USE mp_images,       ONLY: inter_image_comm
+
+    !> Volume of the unit cell
+    REAL(dp),      INTENT(IN)  :: omega
+
+    !> Type that defines the custom Fourier transform for Sigma
+    TYPE(fft_cus), INTENT(IN)  :: fft_cust
+
+    !> If this flag is set, we use the multishift solver
+    LOGICAL,       INTENT(IN)  :: multishift
+
+    !> The l value of the BiCGStab(l) algorithm.
+    INTEGER,       INTENT(IN)  :: lmax
+
+    !> The convergence threshold for the solver.
+    REAL(dp),      INTENT(IN)  :: threshold
+
+    !> The chemical potential of the system.
+    REAL(dp),      INTENT(IN)  :: mu
+
+    !> The prefactor with which the self-energy is multiplied
+    COMPLEX(dp),   INTENT(IN)  :: alpha
+
+    !> The k point at which the Green's function is evaluated
+    REAL(dp),      INTENT(IN)  :: kpt(:)
+
+    !> This type defines the frequencies used for the integration
+    TYPE(freqbins_type), INTENT(IN) :: freq
+
+    !> the first frequency done on this process
+    INTEGER,       INTENT(IN)  :: first_sigma
+
+!    !> The eigenvalues \f$\epsilon\f$ for the occupied bands.
+!    COMPLEX(dp),   INTENT(IN)  :: eval(:)
+!
+!    !> The eigenvectors \f$u_{\text v}(G)\f$ of the occupied bands.
+!    COMPLEX(dp),   INTENT(IN)  :: evec(:,:)
+
+    !> The screened Coulomb interaction in reciprocal space
+    COMPLEX(dp),   INTENT(IN)  :: coulomb(:,:,:)
+
+    !> The self-energy \f$\Sigma\f$ at the specified frequency points
+    COMPLEX(dp),   INTENT(OUT) :: sigma(:,:,:)
+
+    !> the number of real space points used for the correlation
+    INTEGER num_r_corr
+
+    !> The number of plane-waves in the NSCF calculation.
+    INTEGER num_g
+
+    !> the number of G vectors used for the correlation
+    INTEGER num_g_corr
+
+    !> the number of tasks done by this process
+    INTEGER num_task
+
+    !> counter on the number of tasks
+    INTEGER itask
+
+    !> the number of frequencies for the Green's function
+    INTEGER num_green
+
+    !> counter on the Green's function frequencies
+    INTEGER igreen
+
+    !> counter on the Coulomb frequencies
+    INTEGER icoul
+
+    !> Counter for the frequencies of the self energy.
+    INTEGER isigma
+
+    !> The map from G-vectors at current k to global array.
+    INTEGER,     ALLOCATABLE :: map(:)
+
+    !> complex value of the chemical potential
+    COMPLEX(dp) mu_
+
+    !> The scaling prefactor in the integration, this is the product of alpha
+    !! and the weight of the frequency point.
+    COMPLEX(dp) alpha_weight
+
+    !> The current frequency used for the Coulomb interaction
+    COMPLEX(dp) freq_coul
+
+    !> The frequencies used for the Green's function
+    COMPLEX(dp), ALLOCATABLE :: freq_green(:)
+
+    !> The frequencies used for the self energy.
+    COMPLEX(dp), ALLOCATABLE :: freq_sigma(:)
+
+    !> The Green's function in real or reciprocal space
+    COMPLEX(dp), ALLOCATABLE :: green(:,:,:)
+
+    !> work array; contains either W or \f$\Sigma\f$
+    COMPLEX(dp), ALLOCATABLE :: work(:,:)
+
+    !> complex constant of 0
+    COMPLEX(dp), PARAMETER   :: zero = CMPLX(0.0_dp, 0.0_dp, KIND=dp)
+
+    !
+    ! sanity check of the input
+    !
+    num_r_corr = fft_cust%dfftt%nnr
+    num_g_corr = fft_cust%ngmt
+    num_task = SIZE(sigma, 3)
+    IF (SIZE(coulomb, 1) /= num_g_corr) &
+      CALL errore(__FILE__, "screened Coulomb and FFT type inconsistent", 1)
+    IF (SIZE(coulomb, 2) /= num_g_corr) &
+      CALL errore(__FILE__, "screened Coulomb must be a square matrix", 1)
+    IF (SIZE(sigma, 1) /= num_g_corr) &
+      CALL errore(__FILE__, "self energy and FFT type inconsistent", 1)
+    IF (SIZE(sigma, 2) /= num_g_corr) &
+      CALL errore(__FILE__, "self energy must be a square matrix", 1)
+    IF (first_sigma + num_task - 1 > freq%num_sigma()) &
+      CALL errore(__FILE__, "frequency index is out of bounds of the frequency array", 1)
+
+    !!
+    !! 1. shift the frequency grid so that the origin is at the Fermi energy
+    !!
+    mu_ = CMPLX(mu, 0.0_dp)
+    num_green = 2 * freq%num_coul()
+    ALLOCATE(freq_green(num_green))
+    ALLOCATE(freq_sigma(freq%num_sigma()))
+    freq_green = freq%green(mu_)
+    freq_sigma = mu_ + freq%sigma
+
+    !!
+    !! 2. prepare the QE module so that we can evaluate the Green's function
+    !!
+    ! this will allocate map
+    CALL green_prepare(kpt, num_g_corr, map, num_g)
+
+    !!
+    !! 3. evaluate the Green's function of the system
+    !!
+    ! allocate an array that can contain the Fourier transformed quanity
+    ALLOCATE(green(num_r_corr, num_r_corr, num_green))
+    ! after this call, we obtained G(G, G', w)
+    CALL green_function(inter_image_comm, multishift, lmax, threshold, map, &
+                        num_g, freq_green, green)
+
+    !!
+    !! 4. we add the nonanalytic part if on the real axis
+    !!
+!    IF (.NOT. freq%imag_sigma) THEN
+!      CALL green_nonanalytic(map, freq_green, eval, evec, green)
+!    END IF
+
+    !!
+    !! 5. Fourier transform Green's function to real space
+    !!
+    ! the result is G(r, r', w)
+    DO igreen = 1, num_green
+      CALL invfft6('Custom', green(:,:,igreen), fft_cust%dfftt, fft_cust%nlt, omega)
+    END DO ! igreen
+
+    ! create work array
+    ALLOCATE(work(num_r_corr, num_r_corr))
+
+    !!
+    !! 6. distribute the frequencies of \f$\Sigma\f$ across the image
+    !!
+    DO itask = 1, num_task
+      !
+      isigma = first_sigma + itask - 1
+      !
+      DO igreen = 1, num_green
+        !!
+        !! 7. construct W for the frequency \f$\omega^{\Sigma} - \omega^{\text{green}}\f$.
+        !!
+        work = zero
+        freq_coul = freq_sigma(isigma) - freq_green(igreen)
+        ! work will contain W(G, G', wS - wG)
+        CALL construct_w(coulomb, work(1:num_g_corr, 1:num_g_corr), ABS(freq_coul))
+        !!
+        !! 8. convolute G and W
+        !!
+        icoul = MOD(igreen - 1, freq%num_coul()) + 1
+        alpha_weight = alpha * freq%weight(icoul)
+        ! work will contain Sigma(G, G', wS)
+        CALL sigma_prod(omega, fft_cust, alpha_weight, green(:,:,igreen), work)
+        !
+        !!
+        !! 9. add the result to \f$\Sigma\f$
+        !!
+        sigma(:,:,itask) = sigma(:,:,itask) + work(1:num_g_corr, 1:num_g_corr)
+        !
+      END DO ! igreen
+      !
+    END DO ! isigma
+
+  END SUBROUTINE sigma_correlation
 
 END MODULE sigma_module
