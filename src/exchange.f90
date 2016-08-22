3717a1fbd33d050510b3a831aa6cc6c479e66c62
Martin Schlipf <martin.schlipf@gmail.com>
2016-08-22 14:37:11 +0100

    implement the exchange convolution
    evaluate the following summation and add the result to the self-energy
    sum_G'' f_{nk-q} V_q(G'') u*_{nk-q}(G - G'') u_{nk-q}(G'' - G')

--- src/exchange.f90
+++ src/exchange.f90
@@ -60,6 +60,92 @@ MODULE exchange_module
 
   IMPLICIT NONE
 
+  PRIVATE
+
+  !> a number indicating that the map is out of the boundary of the array
+  INTEGER, PARAMETER :: out_of_bound = 0
+
 CONTAINS
+
+  !> Evaluate the convolution over \f$G''\f$.
+  !!
+  !! The individual parts of the exchange self-energy are given as
+  !! \f{equation}{
+  !!   \Sigma_{k,q}^{\text{x}} = f_{nk-q} \sum_{G''}V_q(G'') 
+  !!   u_{nk-q}^\ast(G - G'') u_{nk-q}(G'' - G')~.
+  !! \f}
+  !! This routine adds the current element to the previously calculated ones,
+  !! so that the array contains the sum over all \f$(nq)\f$ in the end.
+  SUBROUTINE exchange_convolution(occupation, coulomb, evec, map, sigma)
+
+    USE constants, ONLY: eps12
+    USE kinds,     ONLY: dp
+
+    !> The occupation \f$f_{nk-q}\f$ of the state \f$(nk-q)\f$.
+    REAL(dp),    INTENT(IN)    :: occupation
+
+    !> The coulomb potential \f$V_q(G'')\f$.
+    REAL(dp),    INTENT(IN)    :: coulomb(:)
+
+    !> The eigenvector \f$u_{nk-q}(G)\f$.
+    COMPLEX(dp), INTENT(IN)    :: evec(:)
+
+    !> A map of two G indices onto the index of their difference, i.e.,
+    !! if \f$G_i - G_j = G_k\f$ then \f$\text{map}(i,j) = k\f$. If an
+    !! element is not a valid index of evec, set it to out_of_bound.
+    INTEGER,     INTENT(IN)    :: map(:,:)
+
+    !> The self-energy containing the sum of all \f$(nq)\f$ parts.
+    COMPLEX(dp), INTENT(INOUT) :: sigma(:,:)
+
+    !> the number of G points
+    INTEGER num_g
+
+    !> counter on the G, G' and G''
+    INTEGER ig, igp, igpp
+
+    !> pointer to G - G'' and G'' - G'
+    INTEGER g_gpp, gpp_gp
+
+    !
+    ! sanity test
+    !
+    ! num_g contains number of G
+    num_g = SIZE(evec)
+    IF (ANY(map > num_g)) &
+      CALL errore(__FILE__, "map points out of the bounds of the array", 1)
+    IF (SIZE(sigma, 1) /= num_g) &
+      CALL errore(__FILE__, "1st dim of self-energy not consistent with eigenvector", 1)
+    IF (SIZE(sigma, 2) /= num_g) &
+      CALL errore(__FILE__, "2nd dim of self-energy not consistent with eigenvector", 1)
+
+    !
+    ! summation over G''
+    !
+    DO igpp = 1, SIZE(coulomb)
+      !
+      ! loop over G'
+      DO igp = 1, num_g
+        !
+        ! skip element if G'' - G' is out of bounds
+        gpp_gp = map(igpp, igp)
+        IF (gpp_gp == out_of_bound) CYCLE
+        !
+        ! loop over G
+        DO ig = 1, num_g
+          !
+          ! skip element if G - G'' is out of bounds
+          g_gpp = map(ig, igpp)
+          IF (g_gpp == out_of_bound) CYCLE
+          !
+          ! sigma += f_{nk-q} V_q(G'') u*_nk-q(G - G'') u_nk-q(G'' - G')
+          sigma(ig, igp) = sigma(ig, igp) + occupation * coulomb(igpp) &
+                                          * CONJG(evec(g_gpp)) * evec(gpp_gp)
+          !
+        END DO ! ig
+      END DO ! igp
+    END DO ! igpp
+
+  END SUBROUTINE exchange_convolution
 
 END MODULE exchange_module
