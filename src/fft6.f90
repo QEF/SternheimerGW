dfbe75ac16bf21d1a40f8a5d61d05c3357adf40c
Martin Schlipf <martin.schlipf@gmail.com>
2016-10-14 12:02:58 +0100

    implement 6d FFT with different grids for r and r'
    API is similar to conventional 6d FFT except that the FFT type and the map
    occur twice. The old 6d FFT is implemented as a call to this new routine
    to avoid code duplication.

--- src/fft6.f90
+++ src/fft6.f90
@@ -32,13 +32,23 @@ MODULE fft6_module
 
   IMPLICIT NONE
 
+  !> interface for the fwfft6 routines
+  INTERFACE fwfft6
+    MODULE PROCEDURE fwfft6_same, fwfft6_diff
+  END INTERFACE fwfft6
+
+  !> interface for the invfft6 routines
+  INTERFACE invfft6
+    MODULE PROCEDURE invfft6_same, invfft6_diff
+  END INTERFACE invfft6
+
 CONTAINS
 
   !> Transform an input array \f$f(r, r')\f$ from real to reciprocal space
   !! \f$f(G, G')\f$.
   !!
-  !! This is done in the following steps:
-  SUBROUTINE fwfft6(grid_type, f, dfft, map, omega)
+  !! This calls internally the fftw6_diff routine.
+  SUBROUTINE fwfft6_same(grid_type, f, dfft, map, omega)
 
     USE kinds,          ONLY: dp
     USE fft_interfaces, ONLY: fwfft
@@ -53,14 +63,62 @@ CONTAINS
     COMPLEX(dp),  INTENT(INOUT) :: f(:,:)
 
     !> FFT descriptor - this defines the way the Fourier transform is
-    !! executed, must be consistent with grid_type
+    !! executed, must be consistent with grid_type (used for both FFT)
     !! @note the code does not check explicitly if dfft and grid_type are compatible
     TYPE(fft_type_descriptor), INTENT(IN) :: dfft
 
     !> Because usually a sphere of G-vectors is used, not all points on the FFT
     !! grid are set. This map points from index of the G-points inside the
-    !! sphere onto their index in the full G-mesh.
+    !! sphere onto their index in the full G-mesh (used for G and G').
     INTEGER,      INTENT(IN)    :: map(:)
+
+    !> volume of the unit cell
+    REAL(dp),     INTENT(IN)    :: omega
+
+    ! do the FFT via the routine which allows for different arguments
+    CALL fwfft6_diff(grid_type, f, dfft, dfft, map, map, omega)
+
+  END SUBROUTINE fwfft6_same
+
+  !> Transform an input array \f$f(r, r')\f$ from real to reciprocal space
+  !! \f$f(G, G')\f$.
+  !!
+  !! This routine allows to have different FFT grids for the coordinates.
+  !!
+  !! This is done in the following steps:
+  SUBROUTINE fwfft6_diff(grid_type, f, dfft, dfft_p, map, map_p, omega)
+
+    USE kinds,          ONLY: dp
+    USE fft_interfaces, ONLY: fwfft
+    USE fft_types,      ONLY: fft_type_descriptor
+
+    !> grid type used for the Fourier transform, this is passed to fwfft;
+    !! check the definition of fwfft for a list of options
+    CHARACTER(*), INTENT(IN)    :: grid_type
+
+    !> *on input*  the array in real space \f$f(r, r')\f$ <br>
+    !! *on output* the array in reciprocal space \f$f(G, G')\f$
+    COMPLEX(dp),  INTENT(INOUT) :: f(:,:)
+
+    !> FFT descriptor - this defines the way the Fourier transform is
+    !! executed, must be consistent with grid_type (used for r/G FFT)
+    !! @note the code does not check explicitly if dfft and grid_type are compatible
+    TYPE(fft_type_descriptor), INTENT(IN) :: dfft
+
+    !> FFT descriptor - this defines the way the Fourier transform is
+    !! executed, must be consistent with grid_type (used for r'/G' FFT)
+    !! @note the code does not check explicitly if dfft and grid_type are compatible
+    TYPE(fft_type_descriptor), INTENT(IN) :: dfft_p
+
+    !> Because usually a sphere of G-vectors is used, not all points on the FFT
+    !! grid are set. This map points from index of the G-points inside the
+    !! sphere onto their index in the full G-mesh (used for G).
+    INTEGER,      INTENT(IN)    :: map(:)
+
+    !> Because usually a sphere of G-vectors is used, not all points on the FFT
+    !! grid are set. This map points from index of the G-points inside the
+    !! sphere onto their index in the full G-mesh (used for G').
+    INTEGER,      INTENT(IN)    :: map_p(:)
 
     !> volume of the unit cell
     REAL(dp),     INTENT(IN)    :: omega
@@ -69,13 +127,13 @@ CONTAINS
     COMPLEX(dp), ALLOCATABLE    :: work(:)
 
     !> number of points in reciprocal space
-    INTEGER num_g
+    INTEGER num_g, num_g_p
 
     !> counter on reciprocal space points
     INTEGER ig
 
     !> number of points in real space
-    INTEGER num_r
+    INTEGER num_r, num_r_p
 
     !> counter on real space points
     INTEGER ir
@@ -84,8 +142,10 @@ CONTAINS
     INTEGER ierr
 
     ! initialize helper variables
-    num_g = SIZE(map)
-    num_r = dfft%nnr
+    num_g   = SIZE(map)
+    num_g_p = SIZE(map_p)
+    num_r   = dfft%nnr
+    num_r_p = dfft_p%nnr
 
     !
     ! sanity test of the input
@@ -93,20 +153,22 @@ CONTAINS
     ! reduced mesh must be within full mesh
     IF (ANY(map > num_r)) &
       CALL errore(__FILE__, "some G vector are outside the mesh of the Fourier transform", 1)
+    IF (ANY(map_p > num_r_p)) &
+      CALL errore(__FILE__, "some G' vector are outside the mesh of the Fourier transform", 1)
     !
     ! check that f has size compatible with dfft
     IF (SIZE(f, 1) /= num_r) &
-      CALL errore(__FILE__, "size of array not compatible with Fourier transform", 1)
-    IF (SIZE(f, 2) /= num_r) &
-      CALL errore(__FILE__, "f must be a square matrix", 1)
+      CALL errore(__FILE__, "size of array not compatible with 1st Fourier transform", 1)
+    IF (SIZE(f, 2) /= num_r_p) &
+      CALL errore(__FILE__, "size of array not compatible with 2nd Fourier transform", 1)
 
     ! create work array
     ALLOCATE(work(num_r), STAT = ierr)
     IF (ierr /= 0) &
-      CALL errore(__FILE__, "error allocating the work array", ierr)
+      CALL errore(__FILE__, "error allocating the work array for 1st FFT", ierr)
 
     ! loop over second index
-    DO ir = 1, num_r
+    DO ir = 1, num_r_p
       !!
       !! 1. we store the conjugate of a row in the work array \f$w(r) = f^\ast(r, r')\f$
       !!
@@ -122,6 +184,12 @@ CONTAINS
       !!
     END DO ! ir
 
+    ! create work array
+    DEALLOCATE(work)
+    ALLOCATE(work(num_r_p), STAT = ierr)
+    IF (ierr /= 0) &
+      CALL errore(__FILE__, "error allocating the work array for 2nd FFT", ierr)
+
     ! loop over first index (now in reciprocal space)
     DO ig = 1, num_g
       !!
@@ -129,23 +197,23 @@ CONTAINS
       !!
       work = f(ig, :)
       !!
-      !! 5. we tranform the work array \f$w(r') \rightarrow w(G')\f$
+      !! 5. we transform the work array \f$w(r') \rightarrow w(G')\f$
       !!
-      CALL fwfft(grid_type, work, dfft)
+      CALL fwfft(grid_type, work, dfft_p)
       !!
       !! 6. extract the G vectors within the sphere \f$f(G, G') = w(G')\f$
       !!
-      f(ig, :num_g) = work(map) * omega
+      f(ig, :num_g_p) = work(map_p) * omega
       !!
     END DO ! ig
 
-  END SUBROUTINE fwfft6
+  END SUBROUTINE fwfft6_diff
 
   !> Transform an input array \f$f(G, G')\f$ from reciprocal to real space
   !! \f$f(r, r')\f$.
   !!
-  !! This is done in the following steps:
-  SUBROUTINE invfft6(grid_type, f, dfft, map, omega)
+  !! This call internally the invfft6_diff routine.
+  SUBROUTINE invfft6_same(grid_type, f, dfft, map, omega)
 
     USE kinds,          ONLY: dp
     USE fft_interfaces, ONLY: invfft
@@ -172,6 +240,54 @@ CONTAINS
     !> volume of the unit cell
     REAL(dp),     INTENT(IN)    :: omega
 
+    ! do the FFT via the routine which allows for different arguments
+    CALL invfft6_diff(grid_type, f, dfft, dfft, map, map, omega)
+    
+  END SUBROUTINE invfft6_same
+
+  !> Transform an input array \f$f(G, G')\f$ from reciprocal to real space
+  !! \f$f(r, r')\f$.
+  !!
+  !! This routine allows to have different FFT grids for the coordinates.
+  !!
+  !! This is done in the following steps:
+  SUBROUTINE invfft6_diff(grid_type, f, dfft, dfft_p, map, map_p, omega)
+
+    USE kinds,          ONLY: dp
+    USE fft_interfaces, ONLY: invfft
+    USE fft_types,      ONLY: fft_type_descriptor
+
+    !> grid type used for the Fourier transform, this is passed to invfft;
+    !! check the definition of invfft for a list of options
+    CHARACTER(*), INTENT(IN)    :: grid_type
+
+    !> *on input*  the array in reciprocal space \f$f(G, G')\f$ <br>
+    !! *on output* the array in real space \f$f(r, r')\f$
+    COMPLEX(dp),  INTENT(INOUT) :: f(:,:)
+
+    !> FFT descriptor - this defines the way the Fourier transform is
+    !! executed, must be consistent with grid_type (used for the r/G FFT)
+    !! @note the code does not check explicitly if dfft and grid_type are compatible
+    TYPE(fft_type_descriptor), INTENT(IN) :: dfft
+
+    !> FFT descriptor - this defines the way the Fourier transform is
+    !! executed, must be consistent with grid_type (used for the r'/G' FFT)
+    !! @note the code does not check explicitly if dfft and grid_type are compatible
+    TYPE(fft_type_descriptor), INTENT(IN) :: dfft_p
+
+    !> Because usually a sphere of G-vectors is used, not all points on the FFT
+    !! grid are set. This map points from index of the G-points inside the
+    !! sphere onto their index in the full G-mesh. (used for the G mesh)
+    INTEGER,      INTENT(IN)    :: map(:)
+
+    !> Because usually a sphere of G-vectors is used, not all points on the FFT
+    !! grid are set. This map points from index of the G-points inside the
+    !! sphere onto their index in the full G-mesh. (used for the G' mesh)
+    INTEGER,      INTENT(IN)    :: map_p(:)
+
+    !> volume of the unit cell
+    REAL(dp),     INTENT(IN)    :: omega
+
     !> complex constant of zero
     COMPLEX(dp),  PARAMETER     :: zero = CMPLX(0.0_dp, 0.0_dp, KIND = dp)
 
@@ -179,13 +295,13 @@ CONTAINS
     COMPLEX(dp), ALLOCATABLE    :: work(:)
 
     !> number of points in reciprocal space
-    INTEGER num_g
+    INTEGER num_g, num_g_p
 
     !> counter on reciprocal space points
     INTEGER ig
 
     !> number of points in real space
-    INTEGER num_r
+    INTEGER num_r, num_r_p
 
     !> counter on real space points
     INTEGER ir
@@ -194,8 +310,10 @@ CONTAINS
     INTEGER ierr
 
     ! initialize helper variables
-    num_g = SIZE(map)
-    num_r = dfft%nnr
+    num_g   = SIZE(map)
+    num_g_p = SIZE(map_p)
+    num_r   = dfft%nnr
+    num_r_p = dfft_p%nnr
 
     !
     ! sanity test of the input
@@ -203,15 +321,17 @@ CONTAINS
     ! reduced mesh must be within full mesh
     IF (ANY(map > num_r)) &
       CALL errore(__FILE__, "some G vector are outside the mesh of the Fourier transform", 1)
+    IF (ANY(map_p > num_r_p)) &
+      CALL errore(__FILE__, "some G' vector are outside the mesh of the Fourier transform", 1)
     !
     ! check that f has size compatible with dfft
     IF (SIZE(f, 1) /= num_r) &
-      CALL errore(__FILE__, "size of array not compatible with Fourier transform", 1)
-    IF (SIZE(f, 2) /= num_r) &
-      CALL errore(__FILE__, "f must be a square matrix", 1)
+      CALL errore(__FILE__, "size of array not compatible with 1st Fourier transform", 1)
+    IF (SIZE(f, 2) /= num_r_p) &
+      CALL errore(__FILE__, "size of array not compatible with 2nd Fourier transform", 1)
 
-    ! create work array
-    ALLOCATE(work(num_r), STAT = ierr)
+    ! create work array for r'/G' FFT
+    ALLOCATE(work(num_r_p), STAT = ierr)
     IF (ierr /= 0) &
       CALL errore(__FILE__, "error allocating the work array", ierr)
 
@@ -222,11 +342,11 @@ CONTAINS
       !!
       ! note - we initialize work, because map only contains a subset of all entries
       work = zero
-      work(map) = f(ig, :num_g) / omega
+      work(map_p) = f(ig, :num_g_p) / omega
       !!
       !! 2. we transform the work array \f$w(G') \rightarrow w(r')\f$
       !!
-      CALL invfft(grid_type, work, dfft)
+      CALL invfft(grid_type, work, dfft_p)
       !!
       !! 3. we extract the values from the work array \f$f(G, r') = w(r')\f$
       !!
@@ -234,8 +354,14 @@ CONTAINS
       !!
     END DO ! ig
 
+    ! create work array for r/G FFT
+    DEALLOCATE(work)
+    ALLOCATE(work(num_r), STAT = ierr)
+    IF (ierr /= 0) &
+      CALL errore(__FILE__, "error allocating the work array", ierr)
+
     ! loop over second index (now in real space)
-    DO ir = 1, num_r
+    DO ir = 1, num_r_p
       !!
       !! 4. we store a row in the work array \f$w(G) = f^\ast(G, r')\f$
       !!
@@ -243,7 +369,7 @@ CONTAINS
       work = zero
       work(map) = CONJG(f(:num_g, ir))
       !!
-      !! 5. we tranform the work array \f$w(G) \rightarrow w(r')\f$
+      !! 5. we transform the work array \f$w(G) \rightarrow w(r')\f$
       !!
       CALL invfft(grid_type, work, dfft)
       !!
@@ -253,6 +379,6 @@ CONTAINS
       !!
     END DO ! ir
 
-  END SUBROUTINE invfft6
+  END SUBROUTINE invfft6_diff
 
 END MODULE fft6_module
