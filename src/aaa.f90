fdf3ecb3b6a79aa19c14aeea2c8bc43b1817e5d5
Martin Schlipf <martin.schlipf@gmail.com>
2017-09-15 15:41:00 +0100

    create dummy module for AAA rational approximation

---
+++ src/aaa.f90
@@ -0,0 +1,31 @@
+!------------------------------------------------------------------------------
+!
+! This file is part of the SternheimerGW code.
+! 
+! Copyright (C) 2010 - 2017
+! Henry Lambert, Martin Schlipf, and Feliciano Giustino
+!
+! SternheimerGW is free software: you can redistribute it and/or modify
+! it under the terms of the GNU General Public License as published by
+! the Free Software Foundation, either version 3 of the License, or
+! (at your option) any later version.
+!
+! SternheimerGW is distributed in the hope that it will be useful,
+! but WITHOUT ANY WARRANTY; without even the implied warranty of
+! MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+! GNU General Public License for more details.
+!
+! You should have received a copy of the GNU General Public License
+! along with SternheimerGW. If not, see
+! http://www.gnu.org/licenses/gpl.html .
+!
+!------------------------------------------------------------------------------ 
+!> Provide the routines necessary to perform a analytic continuation using
+!! the AAA method.
+!!
+!! Reference: Y. Nakatsukasa, O. Sete, L.N. Trefethen, https://arxiv.org/abs/1612.00337
+MODULE aaa_module
+
+CONTAINS
+
+END MODULE aaa_module
